4e6699676d5d0845a39d1d90a56393896ff10c36
Harriet Hall <harriet.hall@madetech.com>
2024-11-27 10:26:56 +0000

    add help text to govRadio component

--- components/GovRadios.vue
+++ components/GovRadios.vue
@@ -3,7 +3,7 @@
 		context: Object
 	});
 
-	const { attrs: { options }, node: { name }, label } = props.context;
+	const { attrs: { options }, node: { name }, label, help} = props.context;
 
 	function handleInput(e) {
 		props.context.node.input(e.target.value);
@@ -16,6 +16,9 @@
 			<legend class="govuk-fieldset__legend govuk-fieldset__legend--m">
 				{{ label }}
 			</legend>
+						<div id="event-name-hint" class="govuk-hint">
+			{{ help }}
+				</div>
 			<p v-if="props.context.state.invalid" class="govuk-error-message">
 				<span class="govuk-visually-hidden">Error:</span> {{ props.context.messages.rule_required.value }}
 			</p>
