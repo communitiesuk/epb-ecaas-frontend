a3743c239b64ff204e93a600e1a7c304e05bc0b2
James Wainwright <james.wainwright@madetech.com>
2024-12-05 16:21:23 +0000

    Add description to usePage composable

--- composables/page.ts
+++ composables/page.ts
@@ -1,5 +1,9 @@
 import pagesData from "~/data/pages";
 
+/**
+ * Returns page object from page data based on the current route path.
+ * @returns An object representing the current page.
+ */
 export function usePage() {
 	const route = useRoute();
 	return pagesData.find(p => p.url === route.path);
