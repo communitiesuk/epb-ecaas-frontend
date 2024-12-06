a741450892525a360eed9eb9a775e32768c8bf8a
James Wainwright <james.wainwright@madetech.com>
2024-12-06 17:45:43 +0000

    Amended HTML for improved semantics

--- components/GovSideNavBar.vue
+++ components/GovSideNavBar.vue
@@ -1,47 +1,64 @@
 <script setup lang="ts">
-import pagesData, { type Page } from "~/data/pages";
-import { ref } from "vue";
+	import pagesData, { type Page } from "~/data/pages";
 
-const parentPages: Array<Page> = pagesData.filter(
-	(page) => page.parentId === "overview"
-);
+	const parentPages: Array<Page> = pagesData.filter(
+		(page) => page.parentId === "overview"
+	);
 
-const openStates = ref(Array(parentPages.length).fill(true));
+	const openStates = ref(Array(parentPages.length).fill(true));
 
-function toggle(index: number) {
-	openStates.value[index] = !openStates.value[index];
-}
+	function toggle(index: number) {
+		openStates.value[index] = !openStates.value[index];
+	}
 
-function isOpen(index: number) {
-	return openStates.value[index];
-}
+	function isOpen(index: number) {
+		return openStates.value[index];
+	}
 </script>
 
+<style scoped>
+	.accordion-nav {
+		width: 100%;
+		max-width: 250px;
+		float: right;
+	}
+
+	.accordion-nav-heading {
+		display: flex;
+		width: 100%;
+		justify-content: space-between;
+		background: transparent;
+		border: none;
+	}
+
+	.accordion-nav-list {
+		list-style-type: none;
+		margin-top: 20px;
+		border-left-width: 1.5px;
+	}
+
+	.accordion-nav-link {
+		margin-bottom: 14px;
+	}
+</style>
+
 <template>
-	<div v-for="(parentPage, index) in parentPages" :key="parentPage.id" class="app-subnav__section govuk-list">
-		<div @click="toggle(index)">
-			<span class="govuk-accordion__section-toggle" data-nosnippet="">
-				<span class="govuk-accordion__section-toggle-text govuk-!-font-size-16 govuk-!-font-weight-bold">
-					{{ parentPage.title }}
-				</span>
-				<span v-if="isOpen(index)">
-					<span class="govuk-accordion-nav__chevron govuk-accordion-nav__chevron--up" style="float: right"></span>
-				
-					<div class="govuk-inset-text"   style="border-left-width: 1.5px"	>
-						<div v-for="page in pagesData">
-							<span v-if="page.parentId === parentPage.id" class="govuk-body-s">
-								<div style="margin-bottom: 14px" >
-								<NuxtLink  class="govuk-link" @click.stop :to="page.url">{{
-									page.title
-								}}</NuxtLink>
-								</div>
-							</span>
-						</div>
-					</div>
-				</span>
-				<span v-else class="govuk-accordion-nav__chevron govuk-accordion-nav__chevron--down"
-					style="float: right"></span>
-			</span>
+	<nav class="accordion-nav">
+		<div v-for="(parentPage, index) in parentPages" :key="parentPage.id" class="app-subnav__section govuk-list">
+			<button class="govuk-accordion__section-toggle accordion-nav-heading" @click="toggle(index)">
+				<span class="govuk-accordion__section-toggle-text govuk-!-font-size-16 govuk-!-font-weight-bold">{{ parentPage.title }}</span>
+				<span v-if="isOpen(index)" class="govuk-accordion-nav__chevron govuk-accordion-nav__chevron--up"></span>
+				<span v-else class="govuk-accordion-nav__chevron govuk-accordion-nav__chevron--down"></span>
+			</button>
+			<ul v-if="isOpen(index)" class="govuk-inset-text accordion-nav-list">
+				<template v-for="page in pagesData" >
+					<li v-if="page.parentId === parentPage.id">
+						<NuxtLink class="govuk-link govuk-body-s accordion-nav-link" @click.stop :to="page.url">
+							{{ page.title }}
+						</NuxtLink>
+					</li>
+				</template>
+			</ul>
 		</div>
-	</div>
+	</nav>
 </template>
