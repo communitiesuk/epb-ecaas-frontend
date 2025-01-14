c7473fd9af363e5e2d7a30aad4d67d1a772d25f7
kpinakula <7438240+kpinakula@users.noreply.github.com>
2025-01-14 16:11:50 +0000

    Additional tests for hot water distribution page
    Co-Authored-By: James Wainwright <139450713+jameswainwright-mt@users.noreply.github.com>
    Co-Authored-By: Christine Horrocks <christine.horrocks@communities.gov.uk>

--- pages/dwelling-details/hot-water-distribution.test.ts
+++ pages/dwelling-details/hot-water-distribution.test.ts
@@ -23,14 +23,13 @@ const state: HotWaterDistributionData = {
 
 describe('Hot water distribution', () => {
   const store = useEcaasStore()
+  const user = userEvent.setup()
 
   afterEach(() => {
     store.$reset()
   })
 
   it('data is saved to store state when form is valid', async () => {
-    const user = userEvent.setup()
-
     await renderSuspended(HotWaterDistribution)
 
     await user.type(screen.getByTestId('name'), 'Pipework Kitchen Sink')
@@ -51,4 +50,44 @@ describe('Hot water distribution', () => {
     expect(complete).toBe(true)
     expect(navigateToMock).toHaveBeenCalledWith('/dwelling-details')
   })
+
+  it('form is prepopulated when data exists in state', async () => {
+    store.$patch({
+			dwellingDetails: {
+				hotWaterDistribution: {
+					data: state
+				}
+			}
+		});
+
+		await renderSuspended(HotWaterDistribution);
+
+    expect((await screen.findByTestId('name') as HTMLInputElement).value).toBe('Pipework Kitchen Sink');
+    expect((await screen.findByTestId('location_internal')).hasAttribute('checked')).toBe(true);
+    expect((await screen.findByTestId('length') as HTMLInputElement).value).toBe('3');
+    expect((await screen.findByTestId('internalDiameter') as HTMLInputElement).value).toBe('30');
+    expect((await screen.findByTestId('externalDiameter') as HTMLInputElement).value).toBe('33');
+    expect((await screen.findByTestId('insulationThickness') as HTMLInputElement).value).toBe('10');
+    expect((await screen.findByTestId('insulationThermalConductivity') as HTMLInputElement).value).toBe('35');
+    expect((await screen.findByTestId('pipeContents_air')).hasAttribute('checked')).toBe(true);
+    expect((await screen.findByTestId('surfaceReflectivity_yes')).hasAttribute('checked')).toBe(true);
+  })
+
+  it('required error messages are displayed when empty form is submitted', async () => {
+    await renderSuspended(HotWaterDistribution);
+
+    await user.click(screen.getByRole('button'));
+
+    expect((await screen.findByTestId('name_error'))).toBeDefined();
+    expect((await screen.findByTestId('location_error'))).toBeDefined();
+    expect((await screen.findByTestId('length_error'))).toBeDefined();
+  });
+
+  it('error summary is displayed when an invalid form in submitted', async () => {
+		await renderSuspended(HotWaterDistribution);
+
+		await user.click(screen.getByRole('button'));
+
+		expect((await screen.findByTestId('hotWaterDistributionErrorSummary'))).toBeDefined();
+	});
 })
