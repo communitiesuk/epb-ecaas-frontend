435d3621b6027a3ad4d66d7c2d878bc35fc5f854
kpinakula <7438240+kpinakula@users.noreply.github.com>
2025-01-16 12:42:37 +0000

    EC-199 Add name field to shading form
    Co-Authored-By: Christine Horrocks <christine.horrocks@communities.gov.uk>

--- pages/dwelling-details/shading/[shading].vue
+++ pages/dwelling-details/shading/[shading].vue
@@ -8,6 +8,14 @@
 	</Head>
 	<h1 class="govuk-heading-l">{{ title }}</h1>
 	<FormKit type="form" :actions="false">
+		<FormKit
+			type="govInputText"
+			label="Name"
+			help="Name this shading so it can be identified later"
+			id="name"
+			name="name"
+			validation="required"
+		/>
 		<FormKit type="govButton" label="Save and continue" />
 	</FormKit>
 </template>
