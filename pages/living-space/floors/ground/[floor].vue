158b9eaf519cc6ba9fc20ffddd54fe3549adfe96
rachowell <rachel.howell@madetech.com>
2025-03-07 16:42:02 +0000

    EC-383 extract mass distribution class into component

--- pages/living-space/floors/ground/[floor].vue
+++ pages/living-space/floors/ground/[floor].vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import MassDistributionClass from "~/components/fields/MassDistributionClass.vue";
+
 const title = "Ground floor";
 const store = useEcaasStore();
 const route = useRoute();
@@ -129,36 +131,7 @@ const { handleInvalidSubmit, errorMessages } = useErrorSummary();
 			validation="required | number | min:0.01 | max:10"
 		/>
 		<FieldsArealHeatCapacity id="kappaValue" name="kappaValue" />
-		<FormKit
-			id="massDistributionClass"
-			type="govRadios"
-			:options="{
-        internal: {
-          label: 'Mass concentrated on internal side',
-          hint: 'Construction with external thermal insulation (main mass component near inside surface), or equivalent'
-        },
-        external: {
-          label: 'Mass concentrated on external side',
-          hint: 'Construction with internal thermal insulation (main mass component near outside surface), or equivalent'
-        },
-        divided: {
-          label: 'Mass divided over internal and external side',
-          hint: 'Construction with thermal insulation in between two main mass components, or equivalent'
-         },
-        equally: {
-          label: 'Mass equally distributed',
-          hint: 'Uninsulated construction (e.g. solid or hollow bricks, heavy or lightweight concrete, or lightweight construction with negligible mass (e.g. steel sandwich panel), or equivalent'
-        },
-        inside: {
-          label: 'Mass concentrated inside',
-           hint: 'Construction with both internal and external insulation (main mass component concentrated near centre of construction), or equivalent'
-           },
-      }"
-			label="Mass distribution class"
-			help="The mass distribution class is used to determine which layers of the construction are relevant to thermal mass heat storage in the model"
-			name="massDistributionClass"
-			validation="required"
-		/>
+    <MassDistributionClass id="massDistributionClass" name="massDistributionClass"/>
 		<FormKit
 			id="perimeter"
 			type="govInputWithSuffix"
--- pages/living-space/walls/party/[wall].vue
+++ pages/living-space/walls/party/[wall].vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import MassDistributionClass from "~/components/fields/MassDistributionClass.vue";
+
 const title = "Party wall";
 const store = useEcaasStore();
 const route = useRoute();
@@ -145,37 +147,7 @@ const { handleInvalidSubmit, errorMessages } = useErrorSummary();
 			validation="required | number | min:0.01 | max:10"
 		/>
 		<FieldsArealHeatCapacity id="kappaValue" name="kappaValue" />
-		<FormKit
-			id="massDistributionClass"
-			type="govRadios"
-			:options="{
-        internal: {
-          label: 'Mass concentrated on internal side',
-          hint: 'Construction with external thermal insulation (main mass component near inside surface), or equivalent'
-        },
-        external: {
-          label: 'Mass concentrated on external side',
-          hint: 'Construction with internal thermal insulation (main mass component near outside surface), or equivalent'
-        },
-        divided: {
-          label: 'Mass divided over internal and external side',
-          hint: 'Construction with thermal insulation in between two main mass components, or equivalent'
-         },
-        equally: {
-          label: 'Mass equally distributed',
-          hint: 'Uninsulated construction (e.g. solid or hollow bricks, heavy or lightweight concrete, or lightweight construction with negligible mass (e.g. steel sandwich panel), or equivalent'
-        },
-        inside: {
-          label: 'Mass concentrated inside',
-           hint: 'Construction with both internal and external insulation (main mass component concentrated near centre of construction), or equivalent'
-           },
-      }"
-			label="Mass distribution class"
-			help="Distribution of mass in building element"
-			name="massDistributionClass"
-			validation="required"
-		/>
-
+    <MassDistributionClass id="massDistributionClass" name="massDistributionClass"/>
 		<FormKit
 			type="govButton"
 			label="Save and continue"
