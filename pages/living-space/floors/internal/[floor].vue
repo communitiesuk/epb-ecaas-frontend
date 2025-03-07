819cedae2b958e848fe92ebf8f383a73769caa35
rachowell <rachel.howell@madetech.com>
2025-03-07 16:00:50 +0000

    EC-383 update text in all instances of mass distribution class

--- pages/living-space/floors/internal/[floor].vue
+++ pages/living-space/floors/internal/[floor].vue
@@ -100,12 +100,27 @@ const { handleInvalidSubmit, errorMessages } = useErrorSummary();
 				id="massDistributionClass"
 				type="govRadios"
 				:options="{
-					internal: 'Mass distributed on internal side',
-					external: 'Mass concentrated on external side',
-					divided: 'Mass divided over internal and external side',
-					equally: 'Mass equally distributed',
-					inside: 'Mass concentrated inside',
-				}"
+          internal: {
+            label: 'Mass concentrated on internal side',
+            hint: 'Construction with external thermal insulation (main mass component near inside surface), or equivalent'
+          },
+          external: {
+            label: 'Mass concentrated on external side',
+            hint: 'Construction with internal thermal insulation (main mass component near outside surface), or equivalent'
+          },
+          divided: {
+            label: 'Mass divided over internal and external side',
+            hint: 'Construction with thermal insulation in between two main mass components, or equivalent'
+           },
+          equally: {
+            label: 'Mass equally distributed',
+            hint: 'Uninsulated construction (e.g. solid or hollow bricks, heavy or lightweight concrete, or lightweight construction with negligible mass (e.g. steel sandwich panel), or equivalent'
+          },
+          inside: {
+            label: 'Mass concentrated inside',
+             hint: 'Construction with both internal and external insulation (main mass component concentrated near centre of construction), or equivalent'
+             },
+        }"
 				label="Mass distribution class"
 				help="Distribution of mass in building element."
 				name="massDistributionClass"
