a91eae164789ce0974eaa8fd2fb764c557c8d277
rachowell <rachel.howell@madetech.com>
2025-03-12 17:16:01 +0000

    EC-336 complete closed fireplace inputs page and unit tests

---
+++ pages/infiltration-and-ventilation/combustion-appliances/closed-fireplace-with-fan/[combustion].vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+
+const title = "Closed fireplace with fan";
+const store = useEcaasStore();
+const route = useRoute();
+
+const applianceData = useItemToEdit('combustion', store.infiltrationAndVentilation.combustionAppliances.closedFireplaceWithFan.data);
+const model: Ref<CombustionApplianceData> = ref(applianceData!);
+
+const saveForm = (fields: CombustionApplianceData) => {
+	store.$patch((state) => {
+		const {combustionAppliances} = state.infiltrationAndVentilation;
+
+		if (!combustionAppliances.closedFireplaceWithFan.data) {
+			combustionAppliances.closedFireplaceWithFan.data = [];
+		}
+
+		const appliance: CombustionApplianceData = {
+			name: fields.name,
+			airSupplyToAppliance: fields.airSupplyToAppliance,
+			exhaustMethodFromAppliance: fields.exhaustMethodFromAppliance,
+			typeOfFuel: fields.typeOfFuel,
+		};
+
+		if (route.params.combustion && route.params.combustion !== 'create') {
+			const index = parseInt(route.params.combustion as string);
+			combustionAppliances.closedFireplaceWithFan.data[index] = appliance;
+		} else {
+			combustionAppliances.closedFireplaceWithFan.data.push(appliance);
+		}
+
+		combustionAppliances.closedFireplaceWithFan.complete = true;
+	});
+
+	navigateTo("/infiltration-and-ventilation/combustion-appliances");
+};
+
+const {handleInvalidSubmit, errorMessages} = useErrorSummary();
+</script>
+
+<template>
+	<Head>
+		<Title>{{ title }}</Title>
+	</Head>
+	<h1 class="govuk-heading-l">{{ title }}</h1>
+	<FormKit
+		v-model="model"
+		type="form"
+		:actions="false"
+		:incomplete-message="false"
+		@submit="saveForm"
+		@submit-invalid="handleInvalidSubmit"
+	>
+		<GovErrorSummary :error-list="errorMessages" test-id="closedFireplaceWithFanErrorSummary"/>
+		<FormKit
+			id="name"
+			type="govInputText"
+			label="Name"
+			help="Provide a name for this element so that it can be identified later"
+			name="name"
+			validation="required"
+		/>
+		<FieldsAirSupplyToAppliance id="airSupplyToAppliance" name="airSupplyToAppliance"/>
+		<FieldsExhaustMethodFromAppliance id="exhaustMethodFromAppliance" name="exhaustMethodFromAppliance"/>
+		<FieldsTypeOfFuel id="typeOfFuel" name="typeOfFuel"/>
+		<FormKit
+			type="govButton"
+			label="Save and continue"
+		/>
+	</FormKit>
+</template>
---
+++ pages/infiltration-and-ventilation/combustion-appliances/closed-fireplace-with-fan/closed-fireplace-with-fan.test.ts
@@ -0,0 +1,77 @@
+import userEvent from "@testing-library/user-event";
+import ClosedFireplaceWithFan from './[combustion].vue';
+import {mockNuxtImport, renderSuspended} from "@nuxt/test-utils/runtime";
+import {screen} from "@testing-library/vue";
+
+const navigateToMock = vi.hoisted(() => vi.fn());
+mockNuxtImport('navigateTo', () => {
+	return navigateToMock;
+});
+
+describe('closed fireplace with fan', () => {
+	const store = useEcaasStore();
+	const user = userEvent.setup();
+
+	const closedFireplace: CombustionApplianceData = {
+		name: 'Closed fireplace',
+		airSupplyToAppliance: 'roomAir',
+		exhaustMethodFromAppliance: 'intoSeparateDuct',
+		typeOfFuel: 'wood',
+	};
+
+	afterEach(() => {
+		store.$reset();
+	});
+
+	it('data is saved to store state when form is valid', async () => {
+		await renderSuspended(ClosedFireplaceWithFan);
+
+		await user.type(screen.getByTestId('name'), 'Closed fireplace');
+		await user.click(screen.getByTestId('airSupplyToAppliance_roomAir'));
+		await user.click(screen.getByTestId('exhaustMethodFromAppliance_intoSeparateDuct'));
+		await user.click(screen.getByTestId('typeOfFuel_wood'));
+
+		await user.tab();
+		await user.click(screen.getByRole('button'));
+
+		const {data, complete} = store.infiltrationAndVentilation.combustionAppliances.closedFireplaceWithFan;
+
+		expect(data[0]).toEqual(closedFireplace);
+		expect(complete).toBe(true);
+		expect(navigateToMock).toHaveBeenCalledWith('/infiltration-and-ventilation/combustion-appliances');
+	});
+
+	it('form is prepopulated when data exists in state', async () => {
+		store.$patch({
+			infiltrationAndVentilation: {
+				combustionAppliances: {
+					closedFireplaceWithFan: {
+						data: [closedFireplace]
+					}
+				}
+			}
+		});
+
+		await renderSuspended(ClosedFireplaceWithFan, {
+			route: {
+				params: {combustion: '0'}
+			}
+		});
+
+		expect((await screen.findByTestId('name') as HTMLInputElement).value).toBe('Closed fireplace');
+		expect((await screen.findByTestId('airSupplyToAppliance_roomAir')).hasAttribute('checked')).toBe(true);
+		expect((await screen.findByTestId('exhaustMethodFromAppliance_intoSeparateDuct')).hasAttribute('checked')).toBe(true);
+		expect((await screen.findByTestId('typeOfFuel_wood')).hasAttribute('checked')).toBe(true);
+	});
+
+	it('required error messages are displayed when empty form is submitted', async () => {
+		await renderSuspended(ClosedFireplaceWithFan);
+
+		await user.click(screen.getByRole('button'));
+
+		expect((await screen.findByTestId('name_error'))).toBeDefined();
+		expect((await screen.findByTestId('airSupplyToAppliance_error'))).toBeDefined();
+		expect((await screen.findByTestId('exhaustMethodFromAppliance_error'))).toBeDefined();
+		expect((await screen.findByTestId('typeOfFuel_error'))).toBeDefined();
+	});
+});
