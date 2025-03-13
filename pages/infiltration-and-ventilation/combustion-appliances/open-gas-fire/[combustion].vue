34bb3086edf3e75f56eab3838c3437a78ee7044a
rachowell <rachel.howell@madetech.com>
2025-03-13 10:03:03 +0000

    EC-336 add open gas fire inputs page and unit tests

---
+++ pages/infiltration-and-ventilation/combustion-appliances/open-gas-fire/[combustion].vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+
+const title = "Open gas fire";
+const store = useEcaasStore();
+const route = useRoute();
+
+const applianceData = useItemToEdit('combustion', store.infiltrationAndVentilation.combustionAppliances.openGasFire.data);
+const model: Ref<CombustionApplianceData> = ref(applianceData!);
+
+const saveForm = (fields: CombustionApplianceData) => {
+	store.$patch((state) => {
+		const {combustionAppliances} = state.infiltrationAndVentilation;
+
+		if (!combustionAppliances.openGasFire.data) {
+			combustionAppliances.openGasFire.data = [];
+		}
+
+		const appliance: CombustionApplianceData = {
+			name: fields.name,
+			airSupplyToAppliance: fields.airSupplyToAppliance,
+			exhaustMethodFromAppliance: fields.exhaustMethodFromAppliance,
+			typeOfFuel: fields.typeOfFuel,
+		};
+
+		if (route.params.combustion && route.params.combustion !== 'create') {
+			const index = parseInt(route.params.combustion as string);
+			combustionAppliances.openGasFire.data[index] = appliance;
+		} else {
+			combustionAppliances.openGasFire.data.push(appliance);
+		}
+
+		combustionAppliances.openGasFire.complete = true;
+	});
+
+	navigateTo("/infiltration-and-ventilation/combustion-appliances");
+};
+
+const {handleInvalidSubmit, errorMessages} = useErrorSummary();
+</script>
+
+<template>
+	<Head>
+		<Title>{{ title }}</Title>
+	</Head>
+	<h1 class="govuk-heading-l">{{ title }}</h1>
+	<FormKit
+		v-model="model"
+		type="form"
+		:actions="false"
+		:incomplete-message="false"
+		@submit="saveForm"
+		@submit-invalid="handleInvalidSubmit"
+	>
+		<GovErrorSummary :error-list="errorMessages" test-id="openGasFireErrorSummary"/>
+		<FormKit
+			id="name"
+			type="govInputText"
+			label="Name"
+			help="Provide a name for this element so that it can be identified later"
+			name="name"
+			validation="required"
+		/>
+		<FieldsAirSupplyToAppliance id="airSupplyToAppliance" name="airSupplyToAppliance"/>
+		<FieldsExhaustMethodFromAppliance id="exhaustMethodFromAppliance" name="exhaustMethodFromAppliance"/>
+		<FieldsTypeOfFuel id="typeOfFuel" name="typeOfFuel"/>
+		<FormKit
+			type="govButton"
+			label="Save and continue"
+		/>
+	</FormKit>
+</template>
---
+++ pages/infiltration-and-ventilation/combustion-appliances/open-gas-fire/open-gas-fire.test.ts
@@ -0,0 +1,77 @@
+import userEvent from "@testing-library/user-event";
+import OpenGasFire from './[combustion].vue';
+import {mockNuxtImport, renderSuspended} from "@nuxt/test-utils/runtime";
+import {screen} from "@testing-library/vue";
+
+const navigateToMock = vi.hoisted(() => vi.fn());
+mockNuxtImport('navigateTo', () => {
+	return navigateToMock;
+});
+
+describe('open gas fire', () => {
+	const store = useEcaasStore();
+	const user = userEvent.setup();
+
+	const openGasFire: CombustionApplianceData = {
+		name: 'Open gas fire 1',
+		airSupplyToAppliance: 'outside',
+		exhaustMethodFromAppliance: 'intoMechanicalVent',
+		typeOfFuel: 'oil',
+	};
+
+	afterEach(() => {
+		store.$reset();
+	});
+
+	it('data is saved to store state when form is valid', async () => {
+		await renderSuspended(OpenGasFire);
+
+		await user.type(screen.getByTestId('name'), 'Open gas fire 1');
+		await user.click(screen.getByTestId('airSupplyToAppliance_outside'));
+		await user.click(screen.getByTestId('exhaustMethodFromAppliance_intoMechanicalVent'));
+		await user.click(screen.getByTestId('typeOfFuel_oil'));
+
+		await user.tab();
+		await user.click(screen.getByRole('button'));
+
+		const {data, complete} = store.infiltrationAndVentilation.combustionAppliances.openGasFire;
+
+		expect(data[0]).toEqual(openGasFire);
+		expect(complete).toBe(true);
+		expect(navigateToMock).toHaveBeenCalledWith('/infiltration-and-ventilation/combustion-appliances');
+	});
+
+	it('form is prepopulated when data exists in state', async () => {
+		store.$patch({
+			infiltrationAndVentilation: {
+				combustionAppliances: {
+					openGasFire: {
+						data: [openGasFire]
+					}
+				}
+			}
+		});
+
+		await renderSuspended(OpenGasFire, {
+			route: {
+				params: {combustion: '0'}
+			}
+		});
+
+		expect((await screen.findByTestId('name') as HTMLInputElement).value).toBe('Open gas fire 1');
+		expect((await screen.findByTestId('airSupplyToAppliance_outside')).hasAttribute('checked')).toBe(true);
+		expect((await screen.findByTestId('exhaustMethodFromAppliance_intoMechanicalVent')).hasAttribute('checked')).toBe(true);
+		expect((await screen.findByTestId('typeOfFuel_oil')).hasAttribute('checked')).toBe(true);
+	});
+
+	it('required error messages are displayed when empty form is submitted', async () => {
+		await renderSuspended(OpenGasFire);
+
+		await user.click(screen.getByRole('button'));
+
+		expect((await screen.findByTestId('name_error'))).toBeDefined();
+		expect((await screen.findByTestId('airSupplyToAppliance_error'))).toBeDefined();
+		expect((await screen.findByTestId('exhaustMethodFromAppliance_error'))).toBeDefined();
+		expect((await screen.findByTestId('typeOfFuel_error'))).toBeDefined();
+	});
+});
