11b5009788ddb6d92c06f65c69ed9a1bd754f674
rachowell <rachel.howell@madetech.com>
2025-03-13 10:10:46 +0000

    EC-336 add closed fire inputs page and unit tests

---
+++ pages/infiltration-and-ventilation/combustion-appliances/closed-fire/closed-fire.test.ts
@@ -0,0 +1,77 @@
+import userEvent from "@testing-library/user-event";
+import ClosedFire from './[combustion].vue';
+import {mockNuxtImport, renderSuspended} from "@nuxt/test-utils/runtime";
+import {screen} from "@testing-library/vue";
+
+const navigateToMock = vi.hoisted(() => vi.fn());
+mockNuxtImport('navigateTo', () => {
+	return navigateToMock;
+});
+
+describe('closed fire', () => {
+	const store = useEcaasStore();
+	const user = userEvent.setup();
+
+	const closedFire: CombustionApplianceData = {
+		name: 'Closed fire 1',
+		airSupplyToAppliance: 'outside',
+		exhaustMethodFromAppliance: 'intoMechanicalVent',
+		typeOfFuel: 'coal',
+	};
+
+	afterEach(() => {
+		store.$reset();
+	});
+
+	it('data is saved to store state when form is valid', async () => {
+		await renderSuspended(ClosedFire);
+
+		await user.type(screen.getByTestId('name'), 'Closed fire 1');
+		await user.click(screen.getByTestId('airSupplyToAppliance_outside'));
+		await user.click(screen.getByTestId('exhaustMethodFromAppliance_intoMechanicalVent'));
+		await user.click(screen.getByTestId('typeOfFuel_coal'));
+
+		await user.tab();
+		await user.click(screen.getByRole('button'));
+
+		const {data, complete} = store.infiltrationAndVentilation.combustionAppliances.closedFire;
+
+		expect(data[0]).toEqual(closedFire);
+		expect(complete).toBe(true);
+		expect(navigateToMock).toHaveBeenCalledWith('/infiltration-and-ventilation/combustion-appliances');
+	});
+
+	it('form is prepopulated when data exists in state', async () => {
+		store.$patch({
+			infiltrationAndVentilation: {
+				combustionAppliances: {
+					closedFire: {
+						data: [closedFire]
+					}
+				}
+			}
+		});
+
+		await renderSuspended(ClosedFire, {
+			route: {
+				params: {combustion: '0'}
+			}
+		});
+
+		expect((await screen.findByTestId('name') as HTMLInputElement).value).toBe('Closed fire 1');
+		expect((await screen.findByTestId('airSupplyToAppliance_outside')).hasAttribute('checked')).toBe(true);
+		expect((await screen.findByTestId('exhaustMethodFromAppliance_intoMechanicalVent')).hasAttribute('checked')).toBe(true);
+		expect((await screen.findByTestId('typeOfFuel_coal')).hasAttribute('checked')).toBe(true);
+	});
+
+	it('required error messages are displayed when empty form is submitted', async () => {
+		await renderSuspended(ClosedFire);
+
+		await user.click(screen.getByRole('button'));
+
+		expect((await screen.findByTestId('name_error'))).toBeDefined();
+		expect((await screen.findByTestId('airSupplyToAppliance_error'))).toBeDefined();
+		expect((await screen.findByTestId('exhaustMethodFromAppliance_error'))).toBeDefined();
+		expect((await screen.findByTestId('typeOfFuel_error'))).toBeDefined();
+	});
+});
