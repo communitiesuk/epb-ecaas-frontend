c21af377f4412f090c219b8a9c5ab471e00524e4
Harriet Hall <harriet.hall@madetech.com>
2025-03-25 12:32:49 +0000

    EC-432 fix test id's for secondary pipework
    Co-Authored-By: Korinna Pinakoulakis <7438240+kpinakula@users.noreply.github.com>

--- pages/domestic-hot-water/pipework/pipework.test.ts
+++ pages/domestic-hot-water/pipework/pipework.test.ts
@@ -1,67 +1,67 @@
 import { renderSuspended } from "@nuxt/test-utils/runtime";
 import userEvent from "@testing-library/user-event";
-import { screen } from '@testing-library/vue';
-import Pipework from './index.vue';
+import { screen } from "@testing-library/vue";
+import Pipework from "./index.vue";
 import type { PipeworkData } from "~/stores/ecaasStore.types";
 
-describe('Pipeworks', () => {
+describe("Pipeworks", () => {
 	const store = useEcaasStore();
 	const user = userEvent.setup();
 
 	const pipework1: PipeworkData = {
-		name: 'Pipework Kitchen Sink',
+		name: "Pipework Kitchen Sink",
 		length: 3,
-		location: 'internal',
+		location: "internal",
 		internalDiameter: 30,
 	};
 
 	const pipework2: PipeworkData = {
-		name: 'Pipework Kitchen',
+		name: "Pipework Kitchen",
 		length: 4,
-		location: 'internal',
+		location: "internal",
 	};
 
 	afterEach(() => {
 		store.$reset();
 	});
 
-	it('pipework is removed when remove link is clicked', async () => {
+	it("pipework is removed when remove link is clicked", async () => {
 		store.$patch({
 			domesticHotWater: {
 				pipework: {
-					data: [pipework1]
-				}
-			}
+					data: [pipework1],
+				},
+			},
 		});
 
 		await renderSuspended(Pipework);
 
-		const populatedList = screen.queryByTestId('pipework_items');
+		const populatedList = screen.queryByTestId("secondary_items");
 
-		await user.click(screen.getByTestId('pipework_remove_0'));
+		await user.click(screen.getByTestId("secondary_remove_0"));
 
 		expect(populatedList).toBeDefined();
-		expect(screen.queryByTestId('pipework_items')).toBeNull();
+		expect(screen.queryByTestId("secondary_items")).toBeNull();
 	});
 
-	it('duplicates pipework in list when duplicate link is clicked', async () => {
+	it("duplicates pipework in list when duplicate link is clicked", async () => {
 		store.$patch({
 			domesticHotWater: {
 				pipework: {
-					data: [pipework1, pipework2]
-				}
-			}
+					data: [pipework1, pipework2],
+				},
+			},
 		});
 
 		await renderSuspended(Pipework);
 
-		await user.click(screen.getByTestId('pipework_duplicate_0'));
-		await user.click(screen.getByTestId('pipework_duplicate_0'));
-		await user.click(screen.getByTestId('pipework_duplicate_1'));
+		await user.click(screen.getByTestId("secondary_duplicate_0"));
+		await user.click(screen.getByTestId("secondary_duplicate_0"));
+		await user.click(screen.getByTestId("secondary_duplicate_1"));
 
-		expect(screen.queryAllByTestId('pipework_item').length).toBe(5);
-		expect(screen.getByText('Pipework Kitchen Sink (1)')).toBeDefined();
-		expect(screen.getByText('Pipework Kitchen Sink (2)')).toBeDefined();
-		expect(screen.getByText('Pipework Kitchen (1)')).toBeDefined();
+		expect(screen.queryAllByTestId("secondary_item").length).toBe(5);
+		expect(screen.getByText("Pipework Kitchen Sink (1)")).toBeDefined();
+		expect(screen.getByText("Pipework Kitchen Sink (2)")).toBeDefined();
+		expect(screen.getByText("Pipework Kitchen (1)")).toBeDefined();
 	});
 });
