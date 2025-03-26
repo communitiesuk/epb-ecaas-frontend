2a0e554f6a3696af9b381de2ddf46a8339372197
Harriet Hall <harriet.hall@madetech.com>
2025-03-26 14:18:05 +0000

    EC-432 add help with input component to pipework
    Co-Authored-By: Korinna Pinakoulakis <7438240+kpinakula@users.noreply.github.com>

--- pages/domestic-hot-water/pipework/secondary/[pipe].vue
+++ pages/domestic-hot-water/pipework/secondary/[pipe].vue
@@ -23,83 +23,71 @@ const saveForm = (fields: PipeworkData) => {
 	navigateTo("/domestic-hot-water/pipework");
 };
 
-const {handleInvalidSubmit, errorMessages} = useErrorSummary();
+const { handleInvalidSubmit, errorMessages } = useErrorSummary();
 </script>
 
 <template>
+
 	<Head>
 		<Title>{{ title }}</Title>
 	</Head>
 	<h1 class="govuk-heading-l">{{ title }}</h1>
 	<FormKit
-		v-model="model"
-		type="form"
-		:actions="false"
-		:incomplete-message="false"
-		@submit="saveForm"
-		@submit-invalid="handleInvalidSubmit"
-	>
-		<GovErrorSummary
-			:error-list="errorMessages"
-			test-id="pipeworkErrorSummary"
-		/>
+		v-model="model" type="form" :actions="false" :incomplete-message="false" @submit="saveForm"
+		@submit-invalid="handleInvalidSubmit">
+		<GovErrorSummary :error-list="errorMessages" test-id="pipeworkErrorSummary" />
 
-		<GovDetails
-			class="summary-text"
-			:summary-text="`Typical input values`"
-			classes="govuk-!-margin-bottom-4">
-			Length = 20m<br>
-			Internal diameter = 0.022m
+		<GovDetails class="summary-text" :summary-text="`Help with these inputs`" classes="govuk-!-margin-bottom-4">
+			<table class="govuk-table">
+				<tbody class="govuk-table__body">
+					<tr class="govuk-table__row">
+						<th class="govuk-table__header">Parameter</th>
+						<th class="govuk-table__header">Typical values</th>
+					</tr>
+					<tr class="govuk-table__row">
+						<td class="govuk-table__cell">Internal diameter of pipework</td>
+						<td class="govuk-table__cell">10 - 22 mm, depending on use: <br >10 - 15 mm → For individual outlets (e.g.,
+							taps, showers)<br >15 - 22 mm → For main hot water
+							distribution pipes</td>
+					</tr>
+					<tr class="govuk-table__row">
+						<td class="govuk-table__cell">Length of pipework</td>
+						<td class="govuk-table__cell">5 - 15 meters → Small apartment or compact layout<br >15 - 30 meters →
+							Standard house with multiple outlets<br >30+ meters → Larger homes with complex pipe runs</td>
+					</tr>
+
+				</tbody>
+			</table>
+
 		</GovDetails>
 
 		<FormKit
-			id="name"
-			type="govInputText"
-			label="Name"
-			help="Provide a name for this element so that it can be identified later"
-			name="name"
-			validation="required | length:1,50"
-		/>
+			id="name" type="govInputText" label="Name"
+			help="Provide a name for this element so that it can be identified later" name="name"
+			validation="required | length:1,50" />
 		<FormKit
-			id="location"
-			type="govRadios"
-			:options="{
+			id="location" type="govRadios" :options="{
 				internal: 'Internal',
 				external: 'External',
-			}"
-			label="Location"
-			help="The location of the pipe"
-			name="location"
-			validation="required"
-		/>
+			}" label="Location" help="The location of the pipe" name="location" validation="required" />
 		<FormKit
-			id="length"
-			type="govInputWithSuffix"
-			label="Length"
+			id="length" type="govInputWithSuffix" label="Length"
 			help="Total length of distribution pipework - pipework serving multiple tapping points should be counted once for each tapping point"
-			name="length"
-			validation="required | number | min:0 | max:200"
-			suffix-text="m"
-		/>
+			name="length" validation="required | number | min:0 | max:200" suffix-text="m" />
 		<FormKit
-			id="internalDiameter"
-			type="govInputWithSuffix"
-			label="Internal diameter"
-			help="Internal diameter of the pipe"
-			name="internalDiameter"
-			validation="number| min:0.005 | max:0.1"
-			suffix-text="m"
-		/>
-		<FormKit type="govButton" label="Save and continue"/>
+			id="internalDiameter" type="govInputWithSuffix" label="Internal diameter"
+			help="Internal diameter of the pipe" name="internalDiameter" validation="number| min:0.005 | max:0.1"
+			suffix-text="m" />
+		<FormKit type="govButton" label="Save and continue" />
 	</FormKit>
 </template>
 
 <style scoped lang="scss">
 .summary-text {
-  white-space: pre-wrap;
+	white-space: pre-wrap;
 }
 
 .h2 {
-  padding-top: 40px;
+	padding-top: 40px;
 }
 </style>
