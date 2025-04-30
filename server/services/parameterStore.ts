05d2b6b1444d0c020d6c54a2692bb1de3fa572ca
James Wainwright <james.wainwright@madetech.com>
2025-04-30 15:20:22 +0100

    EC-522 - Move parameter story query params to URL

--- server/services/parameterStore.ts
+++ server/services/parameterStore.ts
@@ -2,14 +2,10 @@ import type { ParameterStoreResponse } from "../server.types";
 
 const parameterStore = {
 	getParameter: async (name: string) => {
-		const response = await fetch('http://localhost:2773/systemsmanager/parameters/get', {
+		const response = await fetch(`http://localhost:2773/systemsmanager/parameters/get?name=${name}&withDecryption=true`, {
 			headers: {
 				'X-Aws-Parameters-Secrets-Token': process.env.AWS_SESSION_TOKEN!
-			},
-			body: new URLSearchParams({
-				name: name,
-				withDecryption: 'true'
-			})
+			}
 		});
 
 		return await response.json() as ParameterStoreResponse;
