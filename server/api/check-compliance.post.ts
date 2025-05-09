15117e50f742eadb2f88d067ba11a1e302d76a36
Douglas Greenshields <douglas.greenshields@communities.gov.uk>
2025-05-09 15:37:03 +0100

    add temporary console statement to check console use in lambda can generate logs

--- server/api/check-compliance.post.ts
+++ server/api/check-compliance.post.ts
@@ -2,6 +2,8 @@ import clientSession from "../services/clientSession";
 import ecaasApi from "../services/ecaasApi";
 
 export default defineEventHandler(async event => {
+	console.log("checking compliance!");
+
 	const body = await readBody(event);
 
 	const { accessToken } = await clientSession.get();
