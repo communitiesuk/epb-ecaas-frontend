4dbef240dbe3c493ed4652f0070a2e3483dc48c3
Harriet Hall <harriet.hall@madetech.com>
2025-05-15 14:54:56 +0100

    EC-519 - test: heating systems summary page displays correct data for energy supply

--- pages/heating-systems/summary.test.ts
+++ pages/heating-systems/summary.test.ts
@@ -9,11 +9,47 @@ describe("Heating systems summary page", () => {
 	});
 
 	describe("Energy supply section", () => {
+
 		it("should display the correct tabs for energy supply", async () => {
 			await renderSuspended(HeatingSystemsSummary);
 			expect(
 				screen.getByRole("link", { name: "Energy supply" })
 			).not.toBeNull();
+		});
+
+		it("should display the correct data for energy supply", async () => {
+			const store = useEcaasStore();
+
+			const energySupplyData: EnergySupplyData = {
+				fuelType: ["wood", "electricity"],
+				exported: "yes",
+				co2PerKwh: 1,
+				co2PerKwhIncludingOutOfScope: 2,
+				kwhPerKwhDelivered: 3
+			};
+      
+			store.$patch({
+				heatingSystems: {
+					energySupply: {
+						data: energySupplyData
+					},
+				},
+			});
+
+			const expectedEnergySupplyData = {
+				"Fuel type": "WoodElectricity",
+				"Exported": "Yes",
+				"CO2 per kWh": "1",
+				"CO2 per kWh (including out of scope)": "2",
+				"kWh per kWh delivered": "3"
+			};
+			await renderSuspended(HeatingSystemsSummary);
+			for(const [key, value] of Object.entries(expectedEnergySupplyData)){
+				const lineResult = await screen.findByTestId(`summary-energySupply-${hyphenate(key)}`);
+				expect(lineResult.querySelector("dt")?.textContent).toBe(key);
+				expect(lineResult.querySelector("dd")?.textContent).toBe(value);
+			}
+
 		});
 	});
 });
--- pages/heating-systems/summary.vue
+++ pages/heating-systems/summary.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
 import type { SummarySection } from "~/common.types";
 import { getTabItems } from "#imports";
-
+const store = useEcaasStore();
 const title = "Heating system summary";
 
 const energySupplySummary: SummarySection = {
 	id: "energySupply",
-	label: "Energy supply", 
-	data: {},
-	editUrl: ""
+	label: "Energy supply",
+	data: {
+		"Fuel type": store.heatingSystems.energySupply.data.fuelType,
+		"Exported": store.heatingSystems.energySupply.data.exported,
+		"CO2 per kWh": store.heatingSystems.energySupply.data.co2PerKwh,
+		"CO2 per kWh (including out of scope)": store.heatingSystems.energySupply.data.co2PerKwhIncludingOutOfScope,
+		"kWh per kWh delivered": store.heatingSystems.energySupply.data.kwhPerKwhDelivered,
+	},
+	editUrl: "",
 };
 </script>
 <template>
@@ -17,6 +23,9 @@ const energySupplySummary: SummarySection = {
 	</Head>
 	<h1 class="govuk-heading-l">{{ title }}</h1>
 	<GovTabs v-slot="tabProps" :items="getTabItems([energySupplySummary])">
-		<GovSummaryTab :summary="energySupplySummary" :selected="tabProps.currentTab === 0" />
+		<GovSummaryTab
+			:summary="energySupplySummary"
+			:selected="tabProps.currentTab === 0"
+		/>
 	</GovTabs>
 </template>
