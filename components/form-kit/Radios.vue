ec440322f7fc1cab2073cbbddc62531ae4697a26
Douglas Greenshields <douglas.greenshields@communities.gov.uk>
2025-05-16 12:18:01 +0100

    EC-584 separate radios component into formkit agnostic one + HOC integrating formkit

--- components/form-kit/Radios.vue
+++ components/form-kit/Radios.vue
@@ -19,8 +19,6 @@ const {
 	help
 } = props.context;
 
-const { mounted } = useMounted();
-
 function handleInput(e: Event) {
 	const target = e.target as HTMLInputElement;
 	const valueType = props.context.attrs['value-type'];
@@ -36,52 +34,17 @@ const optionsMap = options instanceof Map ? options : new Map(Object.entries(opt
 </script>
 
 <template>
-	<div :class="`govuk-form-group ${showErrorState(props.context) ? 'govuk-form-group--error' : ''}`">
-		<fieldset
-			:id="id"
-			class="govuk-fieldset"
-			:aria-describedby="props.context.state.invalid ? `${id}_error` : help ? `${id}_hint` : ''"
-		>
-			<legend class="govuk-fieldset__legend govuk-fieldset__legend--m">
-				{{ label }}
-			</legend>
-			<div v-if="help" :id="`${id}_hint`" class="govuk-hint">
-				{{ help }}
-			</div>
-			<slot />
-			<p v-if="props.context.state.invalid" class="govuk-error-message" :data-testid="`${id}_error`">
-				<span class="govuk-visually-hidden">Error:</span> {{ getErrorMessage(props.context) }}
-			</p>
-			<div class="govuk-radios govuk-radios--small" data-module="govuk-radios">
-				<div v-for="key in optionsMap.keys()" :key="key" class="govuk-radios__item">
-					<input
-						:id="`${id}_${key}`"
-						class="govuk-radios__input"
-						type="radio"
-						:name="name"
-						:value="key"
-						:checked="mounted ? props.context._value == key : false"
-						:data-testid="`${id}_${key}`"
-						:aria-describedby="typeof optionsMap.get(key) === 'object' ? `${id}_${key}_hint` : ''"
-						@change="handleInput"
-					>
-					<label class="govuk-label govuk-radios__label" :for="`${id}_${key}`">
-						{{ typeof optionsMap.get(key) === 'object' ? optionsMap.get(key).label : optionsMap.get(key) }}
-					</label>
-					<div v-if="(typeof optionsMap.get(key) === 'object')" :id="`${id}_${key}_hint`" class="govuk-hint govuk-radios__hint">
-						{{ optionsMap.get(key).hint }}
-					</div>
-				</div>
-			</div>
-		</fieldset>
-	</div>
+	<GovRadios
+		:id="id"
+		:name="name"
+		:label="label || ''"
+		:help="help"
+		:options="optionsMap"
+		:show-error-state="() => showErrorState(props.context)"
+		:show-error-message="() => getErrorMessage(props.context)"
+		:invalid="props.context.state.invalid"
+		:aria-described-by="props.context.state.invalid ? `${id}_error` : help ? `${id}_hint` : ''"
+		:handle-input="handleInput"
+		:current-value="props.context._value"
+	/>
 </template>
-
-<style lang="scss">
-	.gov-radios-add-link {
-		color: #1d70b8;
-		font-size: 1.1875rem;
-		line-height: 1.3157894737;
-		font-weight: bold;
-	}
-</style>
---
+++ components/gov/Radios.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+
+import type { RadioOption } from '../form-kit/Radios.vue';
+
+interface RadiosProps {
+	id: string;
+	name: string;
+	label: string;
+	help?: string;
+	options: Map<string, string | RadioOption>;
+	showErrorState: () => boolean;
+	invalid: boolean;
+	showErrorMessage: () => string | undefined;
+	ariaDescribedBy?: string;
+	handleInput: (e: Event) => void;
+	currentValue: string | undefined;
+}
+
+const { showErrorState, showErrorMessage } = defineProps<RadiosProps>();
+
+const { mounted } = useMounted();
+
+</script>
+
+<template>
+	<div :class="`govuk-form-group ${showErrorState() ? 'govuk-form-group--error' : ''}`">
+		<fieldset
+			:id="id"
+			class="govuk-fieldset"
+			:aria-describedby="ariaDescribedBy"
+		>
+			<legend class="govuk-fieldset__legend govuk-fieldset__legend--m">
+				{{ label }}
+			</legend>
+			<div v-if="help" :id="`${id}_hint`" class="govuk-hint">
+				{{ help }}
+			</div>
+			<slot />
+			<p v-if="invalid" class="govuk-error-message" :data-testid="`${id}_error`">
+				<span class="govuk-visually-hidden">Error:</span> {{ showErrorMessage() }}
+			</p>
+			<div class="govuk-radios govuk-radios--small" data-module="govuk-radios">
+				<div v-for="key in options.keys()" :key="key" class="govuk-radios__item">
+					<input
+						:id="`${id}_${key}`"
+						class="govuk-radios__input"
+						type="radio"
+						:name="name"
+						:value="key"
+						:checked="mounted ? currentValue == key : false"
+						:data-testid="`${id}_${key}`"
+						:aria-describedby="typeof options.get(key) === 'object' ? `${id}_${key}_hint` : ''"
+						@change="handleInput"
+					>
+					<label class="govuk-label govuk-radios__label" :for="`${id}_${key}`">
+						{{ typeof options.get(key) === 'object' ? (options.get(key) as RadioOption).label : options.get(key) }}
+					</label>
+					<div v-if="(typeof options.get(key) === 'object')" :id="`${id}_${key}_hint`" class="govuk-hint govuk-radios__hint">
+						{{ (options.get(key)as RadioOption).hint }}
+					</div>
+				</div>
+			</div>
+		</fieldset>
+	</div>
+</template>
+
+<style lang="scss">
+	.gov-radios-add-link {
+		color: #1d70b8;
+		font-size: 1.1875rem;
+		line-height: 1.3157894737;
+		font-weight: bold;
+	}
+</style>
