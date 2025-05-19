777e4a45f4669824ba4c0ab5e7953c94c3057f4f
Douglas Greenshields <douglas.greenshields@communities.gov.uk>
2025-05-19 14:16:02 +0100

    EC-584 use explicit type checking for field values in mass distribution class component

--- components/fields/MassDistributionClass.vue
+++ components/fields/MassDistributionClass.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { MassDistributionClass } from '~/schema/api-schema.types';
+import type { RadioOption } from '../form-kit/Radios.vue';
 
 defineProps<{
 	id: string,
@@ -7,34 +8,37 @@ defineProps<{
 	label?: string;
 	help?: string;
 }>();
+
+const options: Record<MassDistributionClass, RadioOption> = {
+	[MassDistributionClass.I]: {
+		label: 'Mass concentrated on internal side',
+		hint: 'Construction with external thermal insulation (main mass component near inside surface), or equivalent'
+	},
+	[MassDistributionClass.E]: {
+		label: 'Mass concentrated on external side',
+		hint: 'Construction with internal thermal insulation (main mass component near outside surface), or equivalent'
+	},
+	[MassDistributionClass.IE]: {
+		label: 'Mass divided over internal and external side',
+		hint: 'Construction with thermal insulation in between two main mass components, or equivalent'
+	},
+	[MassDistributionClass.D]: {
+		label: 'Mass equally distributed',
+		hint: 'Uninsulated construction (e.g. solid or hollow bricks, heavy or lightweight concrete, or lightweight construction with negligible mass (e.g. steel sandwich panel), or equivalent'
+	},
+	[MassDistributionClass.M]: {
+		label: 'Mass concentrated inside',
+		hint: 'Construction with both internal and external insulation (main mass component concentrated near centre of construction), or equivalent'
+	},
+};
+
 </script>
 
 <template>
 	<FormKit
 		:id="id"
 		type="govRadios"
-		:options="{
-			[MassDistributionClass.I]: {
-				label: 'Mass concentrated on internal side',
-				hint: 'Construction with external thermal insulation (main mass component near inside surface), or equivalent'
-			},
-			[MassDistributionClass.E]: {
-				label: 'Mass concentrated on external side',
-				hint: 'Construction with internal thermal insulation (main mass component near outside surface), or equivalent'
-			},
-			[MassDistributionClass.IE]: {
-				label: 'Mass divided over internal and external side',
-				hint: 'Construction with thermal insulation in between two main mass components, or equivalent'
-			},
-			[MassDistributionClass.D]: {
-				label: 'Mass equally distributed',
-				hint: 'Uninsulated construction (e.g. solid or hollow bricks, heavy or lightweight concrete, or lightweight construction with negligible mass (e.g. steel sandwich panel), or equivalent'
-			},
-			[MassDistributionClass.M]: {
-				label: 'Mass concentrated inside',
-				hint: 'Construction with both internal and external insulation (main mass component concentrated near centre of construction), or equivalent'
-			},
-		}"
+		:options="options"
 		:label="label ?? 'Mass distribution class'"
 		:help="help ?? 'Distribution of mass in building element'"
 		:name="name"
