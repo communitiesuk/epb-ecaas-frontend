c30173dd7b5bccc5b62b05956bb6c0f5ec0ffcf7
James Wainwright <james.wainwright@madetech.com>
2025-05-29 13:09:08 +0100

    EC-503 - Move top level awaits to async wrapper handler

--- server/routes/auth/cognito.get.ts
+++ server/routes/auth/cognito.get.ts
@@ -1,29 +1,33 @@
 import type { H3Error, H3Event } from "h3";
 import parameterStore from "~/server/services/parameterStore";
 
-export default defineOAuthCognitoEventHandler({
-	config: {
-		clientId: import.meta.dev ? process.env.NUXT_OAUTH_COGNITO_CLIENT_ID :
-			(await parameterStore.getParameter('nuxt_oauth_cognito_client_id')).Parameter.Value,
+export default defineEventHandler(async event => {
+	const clientId = import.meta.dev ? process.env.NUXT_OAUTH_COGNITO_CLIENT_ID :
+		(await parameterStore.getParameter('nuxt_oauth_cognito_client_id')).Parameter.Value;
 
-		clientSecret: import.meta.dev ? process.env.NUXT_OAUTH_COGNITO_CLIENT_SECRET :
-			(await parameterStore.getParameter('nuxt_oauth_cognito_client_secret')).Parameter.Value,
+	const clientSecret = import.meta.dev ? process.env.NUXT_OAUTH_COGNITO_CLIENT_SECRET :
+		(await parameterStore.getParameter('nuxt_oauth_cognito_client_secret')).Parameter.Value;
 
-		userPoolId: process.env.COGNITO_USER_POOL_ID,
-		region: process.env.AWS_DEFAULT_REGION,
-		scope: ['openid']
-	},
-	async onSuccess(event: H3Event, { user }: { user: { id: unknown } }) {
-		await setUserSession(event, {
-			user: {
-				id: user.id
-			}
-		});
+	return defineOAuthCognitoEventHandler({
+		config: {
+			clientId,
+			clientSecret,
+			userPoolId: process.env.COGNITO_USER_POOL_ID,
+			region: process.env.AWS_DEFAULT_REGION,
+			scope: ['openid']
+		},
+		async onSuccess(event: H3Event, { user }: { user: { id: unknown } }) {
+			await setUserSession(event, {
+				user: {
+					id: user.id
+				}
+			});
 
-		return sendRedirect(event, '/');
-	},
-	onError(event: H3Event, error: H3Error) {
-		console.error('Cognito OAuth error:', error);
-		return sendRedirect(event, '/error');
-	}
+			return sendRedirect(event, '/');
+		},
+		onError(event: H3Event, error: H3Error) {
+			console.error('Cognito OAuth error:', error);
+			return sendRedirect(event, '/error');
+		}
+	})(event);
 });
