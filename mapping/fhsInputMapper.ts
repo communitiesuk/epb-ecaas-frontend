4d629da578ea0b87d47e1696350eebdf5f095f07
Douglas Greenshields <douglas.greenshields@communities.gov.uk>
2025-06-04 16:15:05 +0100

    EC-497 add heating systems mapper to main fhs input mapper

--- mapping/fhsInputMapper.ts
+++ mapping/fhsInputMapper.ts
@@ -4,14 +4,16 @@ import type { SchemaFhsInputSchema } from '~/schema/api-schema.types';
 import { mapDwellingDetailsData } from './dwellingDetailsMapper';
 import merge from 'deepmerge';
 import { mapInfiltrationVentilationData } from './infiltrationVentilationMapper';
+import { mapHeatingSystemsData } from './heatingSystemsMapper';
 
 export function mapFhsInputData(state: EcaasState): FhsInputSchema {
 	const inputData = exampleData as FhsInputSchema;
 
 	const dwellingDetailsData = mapDwellingDetailsData(state);
 	const infiltrationVentilationData = mapInfiltrationVentilationData(state);
+	const heatingSystemsData = mapHeatingSystemsData(state);
 
-	const intermediate = merge(dwellingDetailsData, infiltrationVentilationData);
+	const intermediate = merge(dwellingDetailsData, merge(infiltrationVentilationData, heatingSystemsData));
 	const final = merge(inputData, intermediate);
 
 	console.log(final);
