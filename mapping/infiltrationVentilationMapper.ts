73870b71e1195b49461c3664bc45beaa0e95a012
Douglas Greenshields <douglas.greenshields@communities.gov.uk>
2025-06-04 11:12:43 +0100

    EC-494 rearrange infiltration ventilation mapper module into functions that map 1:1 from forms
    Co-Authored-By: Korinna Pinakoulakis <7438240+kpinakula@users.noreply.github.com>
    Co-Authored-By: Rachel Howell <98879818+rachowell@users.noreply.github.com>

--- mapping/infiltrationVentilationMapper.ts
+++ mapping/infiltrationVentilationMapper.ts
@@ -1,49 +1,35 @@
 import { objectEntries, objectFromEntries, isEmpty } from 'ts-extras';
-import { type CombustionApplianceType, DuctShape, type SchemaCombustionAppliance, type SchemaInfiltrationVentilation, type SchemaMechanicalVentilation, type SchemaMechanicalVentilationDuctwork, type SchemaVent, SupplyAirTemperatureControlType, VentType } from "~/schema/api-schema.types";
+import { type CombustionApplianceType, DuctShape, type SchemaCombustionAppliance, type SchemaInfiltrationVentilation, type SchemaMechanicalVentilation, type SchemaMechanicalVentilationDuctwork, type SchemaVent, type SchemaVentilationLeaks, SupplyAirTemperatureControlType, VentType } from "~/schema/api-schema.types";
 import type { FhsInputSchema } from "./fhsInputMapper";
 import type { InfiltrationFieldsFromDwelling } from "./dwellingDetailsMapper";
 
 export function mapInfiltrationVentilationData(state: EcaasState): Partial<FhsInputSchema> {
-	const infiltrationVentilation = mapMechanicalVentilationData(state);
-
-	return {
-		...infiltrationVentilation
-	};
-}
-
-function mapMechanicalVentilationData(state: EcaasState): Pick<FhsInputSchema, 'InfiltrationVentilation'> {
-	const infiltrationVentilationData = state.infiltrationAndVentilation.ventilation.data;
-	const airPermeabilityData = state.infiltrationAndVentilation.airPermeability.data;
-	const combustionApplianceEntries = objectEntries(state.infiltrationAndVentilation.combustionAppliances).filter(([_key, value]) => value.complete && !isEmpty(value.data)).map(([key, value]) => {
-		return value.data.map<[string, SchemaCombustionAppliance]>((appliance) => {
-			const { name, airSupplyToAppliance, exhaustMethodFromAppliance, typeOfFuel } = appliance;
-			const applianceInput: SchemaCombustionAppliance = {
-				appliance_type: key as CombustionApplianceType,
-				exhaust_situation: exhaustMethodFromAppliance,
-				fuel_type: typeOfFuel,
-				supply_situation: airSupplyToAppliance,
-			};
-			return [name, applianceInput];
-		});
-	}).flat();
-	const combustionAppliances: Record<string, SchemaCombustionAppliance> = objectFromEntries(combustionApplianceEntries);
+	const { dwellingHeight, dwellingEnvelopeArea, dwellingElevationalLevelAtBase, crossVentFactor } = mapVentilationData(state);
+	const mechanicalVentilation = mapMechanicalVentilationData(state);
 
 	const infiltrationVentiliation: Omit<SchemaInfiltrationVentilation, InfiltrationFieldsFromDwelling> = {
 		Cowls: {},
 		PDUs: {},
 		Leaks: {
-			ventilation_zone_height: infiltrationVentilationData.dwellingHeight!,
-			env_area: infiltrationVentilationData.dwellingEnvelopeArea!,
-			test_pressure: airPermeabilityData.testPressure!,
-			test_result: airPermeabilityData.airTightnessTestResult!
+			ventilation_zone_height: dwellingHeight,
+			env_area: dwellingEnvelopeArea,
+			...mapAirPermeabilityData(state),
 		},
-		CombustionAppliances: combustionAppliances,
-		cross_vent_factor: infiltrationVentilationData.crossVentFactor!,
-		ventilation_zone_base_height: infiltrationVentilationData.dwellingElevationalLevelAtBase!,
+		CombustionAppliances: mapCombustionAppliancesData(state),
+		cross_vent_factor: crossVentFactor,
+		ventilation_zone_base_height: dwellingElevationalLevelAtBase,
 		ach_max_static_calcs: 2, // suggested default
 		vent_opening_ratio_init: 1, // 1 is open
-		MechanicalVentilation: mapMechanicalVentilation(state),
-		Vents: mapVents(state)
+		MechanicalVentilation: objectFromEntries(objectEntries(mechanicalVentilation).map(([name, mechanicalVentData]) => {
+			return [
+				name,
+				{
+					...mechanicalVentData,
+					ductwork: mechanicalVentData.vent_type === VentType.MVHR ? mapMvhrDuctworkData(name, state) : undefined
+				}
+			];
+		})),
+		Vents: mapVentsData(state)
 	};
 
 	return {
@@ -51,28 +37,10 @@ function mapMechanicalVentilationData(state: EcaasState): Pick<FhsInputSchema, '
 	} as Pick<FhsInputSchema, 'InfiltrationVentilation'>;
 }
 
-function mapVents(state: EcaasState) {
-	const entries = state.infiltrationAndVentilation.vents.data.map((x): [string, SchemaVent] => {
-		const key = x.name;
-		const val: SchemaVent = {
-			area_cm2: x.effectiveVentilationArea,
-			mid_height_air_flow_path: x.midHeightOfZone,
-			orientation360: x.orientation,
-			pitch: x.pitch,
-			pressure_difference_ref: x.pressureDifference ?? 20 // default if not defined
-		};
-
-		return [key, val];
-	});
-
-	return Object.fromEntries(entries);
-}
-
-function mapMechanicalVentilation(state: EcaasState) {
+function mapMechanicalVentilationData(state: EcaasState) {
 	const entries = state.infiltrationAndVentilation.mechanicalVentilation.data.map((x):[string, SchemaMechanicalVentilation] => {
 		const key = x.name;
-		const mvhr_ductwork: SchemaMechanicalVentilationDuctwork[] = mapMvhrDuctwork(x.name, state);
-		const val: SchemaMechanicalVentilation = {
+		const val: Omit<SchemaMechanicalVentilation, 'ductwork'> = {
 			vent_type: x.typeOfMechanicalVentilationOptions,
 			EnergySupply: "mains elec",
 			design_outdoor_air_flow_rate: x.airFlowRate,
@@ -82,7 +50,6 @@ function mapMechanicalVentilation(state: EcaasState) {
 			mvhr_eff: x.typeOfMechanicalVentilationOptions === VentType.MVHR ? x.mvhrEfficiency : undefined,
 			measured_air_flow_rate: 37,
 			measured_fan_power: 12.26,
-			ductwork: x.typeOfMechanicalVentilationOptions === VentType.MVHR ? mvhr_ductwork : undefined,
 		};
 
 		return [key, val];
@@ -90,7 +57,7 @@ function mapMechanicalVentilation(state: EcaasState) {
 	return Object.fromEntries(entries);
 }
 
-function mapMvhrDuctwork(mechanicalVentilationName: string, state: EcaasState): SchemaMechanicalVentilationDuctwork[] {
+function mapMvhrDuctworkData(mechanicalVentilationName: string, state: EcaasState): SchemaMechanicalVentilationDuctwork[] {
 	const mvhrductworks = state.infiltrationAndVentilation.ductwork?.data.filter(hasMechanicalVentilation);
 
 	function hasMechanicalVentilation(ductwork: DuctworkData) {
@@ -111,4 +78,57 @@ function mapMvhrDuctwork(mechanicalVentilationName: string, state: EcaasState): 
 		};
 		return val;
 	});
+}
+
+function mapVentsData(state: EcaasState) {
+	const entries = state.infiltrationAndVentilation.vents.data.map((x): [string, SchemaVent] => {
+		const key = x.name;
+		const val: SchemaVent = {
+			area_cm2: x.effectiveVentilationArea,
+			mid_height_air_flow_path: x.midHeightOfZone,
+			orientation360: x.orientation,
+			pitch: x.pitch,
+			pressure_difference_ref: x.pressureDifference ?? 20 // default if not defined
+		};
+
+		return [key, val];
+	});
+
+	return objectFromEntries(entries);
+}
+
+function mapVentilationData(state: EcaasState): { dwellingElevationalLevelAtBase: number; dwellingHeight: number; dwellingEnvelopeArea: number; crossVentFactor: boolean; } {
+	const { dwellingElevationalLevelAtBase, dwellingHeight, dwellingEnvelopeArea, crossVentFactor } = state.infiltrationAndVentilation.ventilation.data;
+
+	return {
+		dwellingElevationalLevelAtBase,
+		dwellingHeight,
+		dwellingEnvelopeArea,
+		crossVentFactor
+	};
+}
+
+function mapAirPermeabilityData(state: EcaasState): Pick<SchemaVentilationLeaks, 'test_pressure' | 'test_result'> {
+	const { testPressure, airTightnessTestResult } = state.infiltrationAndVentilation.airPermeability.data;
+
+	return {
+		test_pressure: testPressure,
+		test_result: airTightnessTestResult
+	};
+}
+
+function mapCombustionAppliancesData(state: EcaasState): Record<string, SchemaCombustionAppliance> {
+	const combustionApplianceEntries = objectEntries(state.infiltrationAndVentilation.combustionAppliances).filter(([_key, value]) => value.complete && !isEmpty(value.data)).map(([key, value]) => {
+		return value.data.map<[string, SchemaCombustionAppliance]>((appliance) => {
+			const { name, airSupplyToAppliance, exhaustMethodFromAppliance, typeOfFuel } = appliance;
+			const applianceInput: SchemaCombustionAppliance = {
+				appliance_type: key as CombustionApplianceType,
+				exhaust_situation: exhaustMethodFromAppliance,
+				fuel_type: typeOfFuel,
+				supply_situation: airSupplyToAppliance,
+			};
+			return [name, applianceInput];
+		});
+	}).flat();
+	return objectFromEntries(combustionApplianceEntries);
 }
