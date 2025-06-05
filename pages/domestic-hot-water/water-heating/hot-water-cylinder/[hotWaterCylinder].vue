e0c924090cdd45a798553cb1622d6b6d10f5a378
kpinakula <kpinakula@users.noreply.github.com>
2025-06-05 14:35:04 +0100

    EC-588 Add renamed hot water cylinder files
    Co-Authored-By: Rachel Howell <98879818+rachowell@users.noreply.github.com>

---
+++ pages/domestic-hot-water/water-heating/hot-water-cylinder/[hotWaterCylinder].vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+import { v4 as uuidv4 } from 'uuid';
+import type { HotWaterCylinderData } from '~/stores/ecaasStore.types';
+
+const title = "Hot water cylinder";
+const store = useEcaasStore();
+const { saveToList } = useForm();
+
+const hotWaterCylinderData = useItemToEdit('hotWaterCylinder', store.domesticHotWater.waterHeating.hotWaterCylinder.data);
+const model: Ref<HotWaterCylinderData> = ref(hotWaterCylinderData!);
+
+const saveForm = (fields: HotWaterCylinderData) => {
+	store.$patch((state) => {
+		const {hotWaterCylinder} = state.domesticHotWater.waterHeating;
+
+		const hotWaterCylinderItem: HotWaterCylinderData = {
+			id: uuidv4(),
+			name: fields.name,
+			heatSource: fields.heatSource,
+			tankVolume: fields.tankVolume,
+			dailyEnergyLoss: fields.dailyEnergyLoss
+		};
+
+		saveToList(hotWaterCylinderItem, hotWaterCylinder);
+		hotWaterCylinder.complete = false;
+	});
+
+	navigateTo("/domestic-hot-water/water-heating");
+};
+
+const {handleInvalidSubmit, errorMessages} = useErrorSummary();
+</script>
+
+<template>
+	<Head>
+		<Title>{{ title }}</Title>
+	</Head>
+	<h1 class="govuk-heading-l">{{ title }}</h1>
+	<FormKit
+		v-model="model"
+		type="form"
+		:actions="false"
+		:incomplete-message="false"
+		@submit="saveForm"
+		@submit-invalid="handleInvalidSubmit">
+		<GovErrorSummary :error-list="errorMessages" test-id="hotWaterCylinderErrorSummary"/>
+		<FormKit
+			id="name"
+			type="govInputText"
+			label="Name"
+			help="Provide a name for this element so that it can be identified later"
+			name="name"
+			validation="required"
+		/>
+		<FieldsHeatGenerators
+			id="heatSource"
+			name="heatSource"
+			label="Heat source"
+			help="Select the relevant heat source that has been added previously"
+		/>
+		<FormKit
+			id="tankVolume"
+			type="govInputWithSuffix"
+			label="Tank volume"
+			help="Total internal capacity of the tank in m3"
+			name="tankVolume"
+			validation="required | number | min:0 | max:200"
+			suffix-text="m3"
+		/>
+		<FormKit
+			id="dailyEnergyLoss"
+			type="govInputWithSuffix"
+			label="Daily energy loss"
+			help="Estimated energy lost  from the tank per day"
+			name="dailyEnergyLoss"
+			validation="required | number | min:0 | max:200"
+			suffix-text="kWh/24h"
+		/>
+		<FormKit type="govButton" label="Save and continue" />
+	</FormKit>
+</template>
---
+++ pages/domestic-hot-water/water-heating/hot-water-cylinder/hot-water-cylinder.test.ts
@@ -0,0 +1,120 @@
+import { mockNuxtImport, renderSuspended } from "@nuxt/test-utils/runtime";
+import userEvent from "@testing-library/user-event";
+import { screen } from '@testing-library/vue';
+import HotWaterCylinder from './[hotWaterCylinder].vue';
+import { v4 as uuidv4 } from 'uuid';
+
+const navigateToMock = vi.hoisted(() => vi.fn());
+mockNuxtImport('navigateTo', () => {
+	return navigateToMock;
+});
+
+vi.mock('uuid');
+
+describe('hot water cylinder', () => {
+	const store = useEcaasStore();
+	const user = userEvent.setup();
+
+	const heatPumpId = '463c94f6-566c-49b2-af27-57e5c68b5c30';
+
+	const hotWaterCylinder: HotWaterCylinderData = {
+		id: 'c84528bb-f805-4f1e-95d3-2bd17384fdbe',
+		name: 'Hot water cylinder 1',
+		heatSource: heatPumpId,
+		tankVolume: 5,
+		dailyEnergyLoss: 1,
+	};
+
+	afterEach(() => {
+		store.$reset();
+	});
+
+	const addStoreData = () => {
+		store.$patch({
+			heatingSystems: {
+				heatGeneration: {
+					heatPump: {
+						data: [{
+							id: heatPumpId,
+							name: 'Heat pump'
+						}]
+					}
+				}
+			}
+		});
+	};
+
+	const populateValidForm = async () => {
+		await user.type(screen.getByTestId('name'), 'Hot water cylinder 1');
+		await user.click(screen.getByTestId(`heatSource_${heatPumpId}`));
+		await user.type(screen.getByTestId('tankVolume'), '5');
+		await user.type(screen.getByTestId('dailyEnergyLoss'), '1');
+		await user.tab();
+	};
+
+	it('data is saved to store state when form is valid', async () => {
+		addStoreData();
+
+		vi.mocked(uuidv4).mockReturnValue(hotWaterCylinder.id as unknown as Buffer);
+		await renderSuspended(HotWaterCylinder);
+
+		await populateValidForm();
+		await user.click(screen.getByRole('button'));
+
+		const { data } = store.domesticHotWater.waterHeating.hotWaterCylinder;
+
+		expect(data[0]).toEqual(hotWaterCylinder);
+	});
+
+	it('form is prepopulated when data exists in state', async () => {
+		store.$patch({
+			domesticHotWater: {
+				waterHeating: {
+					hotWaterCylinder: {
+						data: [hotWaterCylinder]
+					}
+				}
+			}
+		});
+
+		addStoreData();
+		await renderSuspended(HotWaterCylinder, {
+			route: {
+				params: { 'hotWaterCylinder': '0' }
+			}
+		});
+
+		expect((await screen.findByTestId('name') as HTMLInputElement).value).toBe('Hot water cylinder 1');
+		expect((await screen.findByTestId('tankVolume') as HTMLInputElement).value).toBe('5');
+		expect((await screen.findByTestId('dailyEnergyLoss') as HTMLInputElement).value).toBe('1');
+	});
+
+	it('required error messages are displayed when empty form is submitted', async () => {
+		await renderSuspended(HotWaterCylinder);
+
+		await user.click(screen.getByRole('button'));
+
+		expect((await screen.findByTestId('name_error'))).toBeDefined();
+		expect((await screen.findByTestId('tankVolume_error'))).toBeDefined();
+		expect((await screen.findByTestId('dailyEnergyLoss_error'))).toBeDefined();
+
+	});
+
+	it('error summary is displayed when an invalid form in submitted', async () => {
+		await renderSuspended(HotWaterCylinder);
+
+		await user.click(screen.getByRole('button'));
+
+		expect((await screen.findByTestId('hotWaterCylinderErrorSummary'))).toBeDefined();
+	});
+
+	it('navigates to hot water outlets page when valid form is completed', async () => {
+		addStoreData();
+		await renderSuspended(HotWaterCylinder);
+
+		await populateValidForm();
+		await user.click(screen.getByRole('button'));
+		
+		expect(navigateToMock).toHaveBeenCalledWith('/domestic-hot-water/water-heating');
+	});
+});
