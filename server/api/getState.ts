be674c587c8a6631c1f22bc96d837986d7926134
Harriet Hall <harriet.hall@madetech.com>
2025-06-16 12:37:15 +0100

    EC-601: Replace Pinia local storage with Valkey cache (localhost)

--- server/api/getState.ts
+++ server/api/getState.ts
@@ -1,20 +1,48 @@
 import { createStorage } from "unstorage";
-import fsDriver from "unstorage/drivers/fs";
-import { EcaasState } from "~/stores/ecaasStore.types";
+import redisDriver from "unstorage/drivers/redis";
+import type { EcaasState } from "~/stores/ecaasStore.types";
+
+// export default defineEventHandler(async (event) => {
+// Read session Id from cookie
+// const sessionId = getCookie(event, "sessionId");
+
+// If session exists, create storage and return existing state
+// if (sessionId) {
+// const storage = createStorage({
+//   driver: redisDriver({
+//     base: "{unstorage}",
+//     cluster: [
+//       {
+//         port: 6379,
+//         host: "elasticache-for-valkey-4oy4a5.serverless.euw2.cache.amazonaws.com:6379",
+//       },
+//     ],
+//   }),
+// });
+
+// return await storage.getItem<EcaasState>(sessionId);
+// }
+
+// Otherwise return null
+//   return null;
+// });
+
 
 export default defineEventHandler(async (event) => {
-	// Read session Id from cookie
-	const sessionId = getCookie(event, 'sessionId');
 
-	// If session exists, create storage and return existing state
-	if (sessionId) {
+	const sessionId = getCookie(event, "sessionId");
+
+	if(sessionId){
+
 		const storage = createStorage({
-			driver: fsDriver({ base: './tmp' })
+			driver: redisDriver({
+				base: "{unstorage}",
+				port: 6379,
+				host: "localhost",
+			}),
 		});
-
+  
 		return await storage.getItem<EcaasState>(sessionId);
 	}
-
-	// Otherwise return null
 	return null;
 });
