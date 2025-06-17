fe3e3d3e259c84fd25d02d1230193cc62ad57635
Douglas Greenshields <dgreenshields@gmail.com>
2025-06-17 22:50:32 +0100

    simplify api access so access token does not need to be passed in separately

--- server/api/check-compliance.post.ts
+++ server/api/check-compliance.post.ts
@@ -1,10 +1,7 @@
-import clientSession from "../services/clientSession";
 import ecaasApi from "../services/ecaasApi";
 
 export default defineEventHandler(async event => {
 	const body = await readBody(event);
 
-	const { accessToken } = await clientSession.get();
-
-	return await ecaasApi.checkCompliance(body, accessToken);
+	return await ecaasApi.checkCompliance(body);
 });
