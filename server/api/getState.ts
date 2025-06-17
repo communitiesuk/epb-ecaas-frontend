ffb0f8c045b36ce36607c9d63a0b5477701b0464
Harriet Hall <harriet.hall@madetech.com>
2025-06-17 14:14:50 +0100

    EC-601: Update the unStorage configuration so data is stored in the ElastiCache cache

--- server/api/getState.ts
+++ server/api/getState.ts
@@ -2,32 +2,6 @@ import { createStorage } from "unstorage";
 import redisDriver from "unstorage/drivers/redis";
 import type { EcaasState } from "~/stores/ecaasStore.types";
 
-// export default defineEventHandler(async (event) => {
-// Read session Id from cookie
-// const sessionId = getCookie(event, "sessionId");
-
-// If session exists, create storage and return existing state
-// if (sessionId) {
-// const storage = createStorage({
-//   driver: redisDriver({
-//     base: "{unstorage}",
-//     cluster: [
-//       {
-//         port: 6379,
-//         host: "elasticache-for-valkey-4oy4a5.serverless.euw2.cache.amazonaws.com",
-//       },
-//     ],
-//   }),
-// });
-
-// return await storage.getItem<EcaasState>(sessionId);
-// }
-
-// Otherwise return null
-//   return null;
-// });
-
-
 export default defineEventHandler(async (event) => {
 
 	const sessionId = getCookie(event, "sessionId");
@@ -37,8 +11,12 @@ export default defineEventHandler(async (event) => {
 		const storage = createStorage({
 			driver: redisDriver({
 				base: "{unstorage}",
-				port: 6379,
-				host: "localhost",
+				cluster: [
+					{
+						port: 6379,
+						host: "elasticache-for-valkey-4oy4a5.serverless.euw2.cache.amazonaws.com",
+					},
+				],
 			}),
 		});
   
--- server/api/setState.ts
+++ server/api/setState.ts
@@ -4,52 +4,6 @@ import type { EcaasState } from "~/stores/ecaasStore.types";
 import { v4 as uuidv4 } from "uuid";
 import merge from "deepmerge";
 
-// export default defineEventHandler(async (event) => {
-
-// Read payload from request body
-// const body = await readBody(event);
-
-// Ready session Id from cookie
-// let sessionId = getCookie(event, 'sessionId');
-
-// Create storage
-
-// const storage = createStorage({
-//   driver: redisDriver({
-//     base: "{unstorage}",
-//     cluster: [
-//       {
-//         port: 6379,
-//         host: "elasticache-for-valkey-4oy4a5.serverless.euw2.cache.amazonaws.com",
-//       },
-//     ],
-//   }),
-// });
-
-// If session exists, read state from storage
-// if (sessionId) {
-//  const state = await storage.getItem<EcaasState>(sessionId);
-
-//  // Merge updated partial state with existing state
-//  const newState = merge(state!, body);
-
-//  // Update storage item
-//  storage.setItem<EcaasState>(sessionId, newState);
-// } else {
-//  // Otherwise create new session Id
-//  sessionId = uuidv4();
-
-//  // Store session Id in cookie
-//  setCookie(event, 'sessionId', sessionId, { maxAge: 60 * 60 * 24 });
-
-//  // Save state to storage
-// await storage.setItem<EcaasState>(sessionId, body, {ttl: 1209600});
-// }
-// return await storage.getItem<EcaasState>(sessionId);
-// event.node.res.end();
-
-// });
-
 export default defineEventHandler(async (event) => {
 	// Read payload from request body
 	const body = await readBody(event);
@@ -61,11 +15,14 @@ export default defineEventHandler(async (event) => {
 	const storage = createStorage({
 		driver: redisDriver({
 			base: "{unstorage}",
-			port: 6379,
-			host: "localhost",
+			cluster: [
+				{
+					port: 6379,
+					host: "elasticache-for-valkey-4oy4a5.serverless.euw2.cache.amazonaws.com",
+				},
+			],
 		}),
 	});
-
 	// If session exists, read state from storage
 	if (sessionId) {
 		const state = await storage.getItem<EcaasState>(sessionId);
