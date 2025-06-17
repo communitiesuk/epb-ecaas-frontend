7c2b2feae23fe45d6921998da09474ca1b90e59b
Harriet Hall <harriet.hall@madetech.com>
2025-06-17 16:42:06 +0100

    EC-601: remove cluser parameter from redisDriver block

--- server/api/getState.ts
+++ server/api/getState.ts
@@ -3,24 +3,18 @@ import redisDriver from "unstorage/drivers/redis";
 import type { EcaasState } from "~/stores/ecaasStore.types";
 
 export default defineEventHandler(async (event) => {
+  const sessionId = getCookie(event, "sessionId");
 
-	const sessionId = getCookie(event, "sessionId");
+  if (sessionId) {
+    const storage = createStorage({
+      driver: redisDriver({
+        base: "{unstorage}",
+        port: 6379,
+        host: "elasticache-for-valkey-4oy4a5.serverless.euw2.cache.amazonaws.com",
+      }),
+    });
 
-	if(sessionId){
-
-		const storage = createStorage({
-			driver: redisDriver({
-				base: "{unstorage}",
-				cluster: [
-					{
-						port: 6379,
-						host: "elasticache-for-valkey-4oy4a5.serverless.euw2.cache.amazonaws.com",
-					},
-				],
-			}),
-		});
-  
-		return await storage.getItem<EcaasState>(sessionId);
-	}
-	return null;
+    return await storage.getItem<EcaasState>(sessionId);
+  }
+  return null;
 });
--- server/api/setState.ts
+++ server/api/setState.ts
@@ -5,45 +5,41 @@ import { v4 as uuidv4 } from "uuid";
 import merge from "deepmerge";
 
 export default defineEventHandler(async (event) => {
-	// Read payload from request body
-	const body = await readBody(event);
+  // Read payload from request body
+  const body = await readBody(event);
 
-	// Ready session Id from cookie
-	let sessionId = getCookie(event, "sessionId");
+  // Ready session Id from cookie
+  let sessionId = getCookie(event, "sessionId");
 
-	// Create storage
-	const storage = createStorage({
-		driver: redisDriver({
-			base: "{unstorage}",
-			cluster: [
-				{
-					port: 6379,
-					host: "elasticache-for-valkey-4oy4a5.serverless.euw2.cache.amazonaws.com",
-				},
-			],
-		}),
-	});
-	// If session exists, read state from storage
-	if (sessionId) {
-		const state = await storage.getItem<EcaasState>(sessionId);
+  // Create storage
+  const storage = createStorage({
+    driver: redisDriver({
+      base: "{unstorage}",
+      port: 6379,
+      host: "elasticache-for-valkey-4oy4a5.serverless.euw2.cache.amazonaws.com",
+    }),
+  });
+  // If session exists, read state from storage
+  if (sessionId) {
+    const state = await storage.getItem<EcaasState>(sessionId);
 
-		// Merge updated partial state with existing state
-		const newState = merge(state!, body);
+    // Merge updated partial state with existing state
+    const newState = merge(state!, body);
 
-		// Update storage item
-		storage.setItem<EcaasState>(sessionId, newState);
-	} else {
-		// Otherwise create new session Id
-		sessionId = uuidv4();
+    // Update storage item
+    storage.setItem<EcaasState>(sessionId, newState);
+  } else {
+    // Otherwise create new session Id
+    sessionId = uuidv4();
 
-		// Store session Id in cookie
-		setCookie(event, "sessionId", sessionId, {
-			maxAge: 60 * 60 * 24 * 14,
-			httpOnly: true,
-		});
+    // Store session Id in cookie
+    setCookie(event, "sessionId", sessionId, {
+      maxAge: 60 * 60 * 24 * 14,
+      httpOnly: true,
+    });
 
-		// Save state to storage
-		await storage.setItem<EcaasState>(sessionId, body, { ttl: 1209600 });
-	}
-	event.node.res.end();
+    // Save state to storage
+    await storage.setItem<EcaasState>(sessionId, body, { ttl: 1209600 });
+  }
+  event.node.res.end();
 });
