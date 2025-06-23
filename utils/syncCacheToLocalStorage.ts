f03a9e9a782eaf715ff7979ef05d5c9308f3fc40
Harriet Hall <harriet.hall@madetech.com>
2025-06-23 14:23:29 +0100

    EC-603: refactor syncCacheToLocalStorage

--- utils/syncCacheToLocalStorage.ts
+++ utils/syncCacheToLocalStorage.ts
@@ -1,10 +1,10 @@
 export const syncCacheToLocalStorage = async () => {
 
-	if(localStorage.getItem("ecaas")){
-		return;
-	}
-	const cachedData = await $fetch("/api/getState",{ method: "GET"});
-	if(cachedData) {
-		localStorage.setItem("ecaas", JSON.stringify(cachedData));
-	}
+	if(!localStorage.getItem("ecaas")){
+
+		const cachedData = await $fetch("/api/getState",{ method: "GET"});
+		if(cachedData) {
+			localStorage.setItem("ecaas", JSON.stringify(cachedData));
+		}
+	};
 };
