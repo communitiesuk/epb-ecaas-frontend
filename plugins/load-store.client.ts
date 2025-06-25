e46865e2804d7cc61a66372fa08cd5d134e397a5
Harriet Hall <harriet.hall@madetech.com>
2025-06-25 08:39:35 +0100

    EC-603: call syncCacheToLocalStorage inside nuxt plugin

--- plugins/load-store.client.ts
+++ plugins/load-store.client.ts
@@ -1,18 +1,3 @@
-// export default defineNuxtPlugin(async() => {
-// 	const store = useEcaasStore();
-// 	try {
-
-// 		const cachedData = await $fetch("/api/getState", {
-// 			method: "GET",
-// 		});
-// 		if (cachedData){
-// 			store.$patch(cachedData);
-
-// 		} else {
-// 			console.log("No cached data found");
-// 		}
-// 	} catch (error){
-// 		console.error("Failed to get data from cache:", error);
-// 	}
-// });
-
+export default defineNuxtPlugin(async () => {
+	await syncCacheToLocalStorage();
+});
