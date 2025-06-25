94b8f0281f24da862148080d6af8ffc3f316e58e
Harriet Hall <harriet.hall@madetech.com>
2025-06-25 18:37:30 +0100

    EC-647: revert to using useRuntimeConfig to retrieve redis endpoint at runtime

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,147 +4,134 @@ import yn from "yn";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: "2024-04-03",
-	devtools: {
-		enabled:
+  compatibilityDate: "2024-04-03",
+  devtools: {
+    enabled:
       typeof process.env.DISABLE_DEVTOOLS !== "undefined"
-      	? !yn(process.env.DISABLE_DEVTOOLS)
-      	: true,
-	},
-	app: {
-		head: {
-			titleTemplate: "%s - ECaaS GOV.UK",
-			meta: [
-				{
-					name: "viewport",
-					content: "width=device-width, initial-scale=1, viewport-fit=cover",
-				},
-				{ name: "theme-color", content: "#0b0c0c" },
-			],
-			link: [
-				{
-					rel: "icon",
-					sizes: "48x48",
-					href: "/static/assets/images/favicon.ico",
-				},
-				{
-					rel: "icon",
-					sizes: "any",
-					href: "/static/assets/images/favicon.svg",
-					type: "image/svg+xml",
-				},
-				{
-					rel: "mask-icon",
-					href: "/static/assets/images/govuk-icon-mask.svg",
-					color: "#0b0c0c",
-				},
-				{
-					rel: "apple-touch-icon",
-					href: "/static/assets/images/govuk-icon-180.png",
-				},
-				{ rel: "manifest", href: "/static/assets/manifest.json" },
-			],
-			bodyAttrs: {
-				class: "govuk-template__body js-enabled govuk-frontend-supported",
-			},
-			htmlAttrs: {
-				class: "govuk-template govuk-template--rebranded",
-			},
-		},
-	},
-	css: ["~/assets/scss/main.scss"],
-	vite: {
-		css: {
-			preprocessorOptions: {
-				scss: {
-					silenceDeprecations: [
-						"mixed-decls",
-						"global-builtin",
-						"slash-div",
-						"import",
-					],
-					additionalData: `
+        ? !yn(process.env.DISABLE_DEVTOOLS)
+        : true,
+  },
+  app: {
+    head: {
+      titleTemplate: "%s - ECaaS GOV.UK",
+      meta: [
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1, viewport-fit=cover",
+        },
+        { name: "theme-color", content: "#0b0c0c" },
+      ],
+      link: [
+        {
+          rel: "icon",
+          sizes: "48x48",
+          href: "/static/assets/images/favicon.ico",
+        },
+        {
+          rel: "icon",
+          sizes: "any",
+          href: "/static/assets/images/favicon.svg",
+          type: "image/svg+xml",
+        },
+        {
+          rel: "mask-icon",
+          href: "/static/assets/images/govuk-icon-mask.svg",
+          color: "#0b0c0c",
+        },
+        {
+          rel: "apple-touch-icon",
+          href: "/static/assets/images/govuk-icon-180.png",
+        },
+        { rel: "manifest", href: "/static/assets/manifest.json" },
+      ],
+      bodyAttrs: {
+        class: "govuk-template__body js-enabled govuk-frontend-supported",
+      },
+      htmlAttrs: {
+        class: "govuk-template govuk-template--rebranded",
+      },
+    },
+  },
+  css: ["~/assets/scss/main.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          silenceDeprecations: [
+            "mixed-decls",
+            "global-builtin",
+            "slash-div",
+            "import",
+          ],
+          additionalData: `
 																								@use "/node_modules/govuk-frontend/dist/govuk/settings/colours-palette" as *;
 																								@use "/node_modules/govuk-frontend/dist/govuk/settings/media-queries" as *;`,
-				},
-			},
-		},
-		plugins: [
-			viteStaticCopy({
-				targets: [
-					{
-						src: "node_modules/govuk-frontend/dist/govuk/assets/*",
-						dest: "static/assets",
-					},
-				],
-			}),
-		],
-	},
-	modules: [
-		"@formkit/nuxt",
-		"@pinia/nuxt",
-		"pinia-plugin-persistedstate/nuxt",
-		"@nuxt/test-utils/module",
-		"@nuxt/eslint",
-		"nuxt-auth-utils",
-		"@nuxtjs/robots",
-	],
-	plugins: [
-		"~/plugins/xray-fetch.server",
-		// '~/plugins/load-store.client',
-		// '~/plugins/update-store.client',
-	],
-	formkit: {
-		autoImport: true,
-		configFile: "./formkit.config.ts",
-	},
-	nitro: process.env.BUILD_FOR_AWS_LAMBDA
-		? {
-			preset: "aws-lambda",
-			storage: {
-				cache: {
-					driver: "redis",
-					port: 6379,
-					host: process.env.ELASTICACHE_ENDPOINT,
+        },
+      },
+    },
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          {
+            src: "node_modules/govuk-frontend/dist/govuk/assets/*",
+            dest: "static/assets",
+          },
+        ],
+      }),
+    ],
+  },
+  modules: [
+    "@formkit/nuxt",
+    "@pinia/nuxt",
+    "pinia-plugin-persistedstate/nuxt",
+    "@nuxt/test-utils/module",
+    "@nuxt/eslint",
+    "nuxt-auth-utils",
+    "@nuxtjs/robots",
+  ],
+  plugins: [
+    "~/plugins/xray-fetch.server",
+    // '~/plugins/load-store.client',
+    // '~/plugins/update-store.client',
+  ],
+  formkit: {
+    autoImport: true,
+    configFile: "./formkit.config.ts",
+  },
+  runtimeConfig: {
+    redisEndpoint: ''
+  },
+  nitro: process.env.BUILD_FOR_AWS_LAMBDA
+    ? {
+        preset: "aws-lambda",
+      }
+    : undefined,
+  typescript: {
+    typeCheck: true,
+  },
+  experimental: {
+    watcher: "parcel",
+  },
+  hooks: {
+    "pages:extend"(pages) {
+      function setAuthMiddleware(pages: NuxtPage[]) {
+        for (const page of pages) {
+          page.meta ||= {};
+          page.meta.middleware = ["authenticated"];
 
-					tls: true,
-				},
-			},
-		}
-		: {
-			devStorage: {
-				cache: {
-					driver: "fs",
-					base: "./tmp",
-				},
-			},
-		},
-	typescript: {
-		typeCheck: true,
-	},
-	experimental: {
-		watcher: "parcel",
-	},
-	hooks: {
-		"pages:extend"(pages) {
-			function setAuthMiddleware(pages: NuxtPage[]) {
-				for (const page of pages) {
-					page.meta ||= {};
-					page.meta.middleware = ["authenticated"];
+          if (page.children) {
+            setAuthMiddleware(page.children);
+          }
+        }
+      }
 
-					if (page.children) {
-						setAuthMiddleware(page.children);
-					}
-				}
-			}
-
-			if (process.env.NODE_ENV === "production") {
-				setAuthMiddleware(pages);
-			}
-		},
-	},
-	site: {
-		indexable: false,
-	},
-	ignore: ["**/*.test.ts"],
+      if (process.env.NODE_ENV === "production") {
+        setAuthMiddleware(pages);
+      }
+    },
+  },
+  site: {
+    indexable: false,
+  },
+  ignore: ["**/*.test.ts"],
 });
---
+++ server/plugins/storage.ts
@@ -0,0 +1,17 @@
+import redisDriver from "unstorage/drivers/redis";
+
+
+export default defineNitroPlugin(() => {
+	const storage = useStorage();
+
+	if (process.env.NODE_ENV === "production") {
+		const driver = redisDriver({
+			base: "unstorage",
+			port: 6379,
+			host: useRuntimeConfig().redisEndpoint,
+			// tls: true as any,
+		});
+
+		storage.mount("cache", driver);
+	}
+});
