56f76daf4c0897156e399b5a0042eef30e7b30a6
Harriet Hall <harriet.hall@madetech.com>
2025-06-26 10:14:58 +0100

    EC-647: update the redis driver configuration to align with ElastiCache cluster requirements

--- server/plugins/storage.ts
+++ server/plugins/storage.ts
@@ -1,15 +1,22 @@
 import redisDriver from "unstorage/drivers/redis";
 
-
 export default defineNitroPlugin(() => {
 	const storage = useStorage();
 
 	if (process.env.NODE_ENV === "production") {
 		const driver = redisDriver({
-			base: "unstorage",
-			port: 6379,
-			host: useRuntimeConfig().redisEndpoint,
-			tls: {},
+			base: "{unstorage}",
+			cluster: [
+				{
+					port: 6379,
+					host: useRuntimeConfig().redisEndpoint,
+				},
+			],
+			clusterOptions: {
+				redisOptions: {
+					tls: {},
+				},
+			},
 		});
 
 		storage.mount("cache", driver);
