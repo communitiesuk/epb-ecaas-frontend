1f40495511844750be62993196e7267d167116ef
Harriet Hall <harriet.hall@madetech.com>
2025-06-26 13:59:42 +0100

    EC-647: Add Redis password to driver config for ElastiCache authentication

--- server/plugins/storage.ts
+++ server/plugins/storage.ts
@@ -1,24 +1,25 @@
 import redisDriver from "unstorage/drivers/redis";
 
 export default defineNitroPlugin(() => {
-	const storage = useStorage();
+  const storage = useStorage();
 
-	if (process.env.NODE_ENV === "production") {
-		const driver = redisDriver({
-			base: "{unstorage}",
-			cluster: [
-				{
-					port: 6379,
-					host: useRuntimeConfig().redisEndpoint,
-				},
-			],
-			clusterOptions: {
-				redisOptions: {
-					tls: {},
-				},
-			},
-		});
+  if (process.env.NODE_ENV === "production") {
+    const driver = redisDriver({
+      base: "{unstorage}",
+      cluster: [
+        {
+          port: 6379,
+          host: useRuntimeConfig().redisEndpoint,
+        },
+      ],
+      clusterOptions: {
+        redisOptions: {
+          tls: {},
+					password: useRuntimeConfig().redisPassword,
+        },
+      },
+    });
 
-		storage.mount("cache", driver);
-	}
+    storage.mount("cache", driver);
+  }
 });
