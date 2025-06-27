eb0949c159700e0e9f2fbfd0a0502f799c55d8db
Harriet Hall <harriet.hall@madetech.com>
2025-06-27 15:38:25 +0100

    add a console.log to debug storage sync issue

--- server/api/getState.ts
+++ server/api/getState.ts
@@ -2,8 +2,12 @@ import type { EcaasState } from "~/stores/ecaasStore.types";
 
 export default defineEventHandler(async (event) => {
 	const sessionId = getCookie(event, "sessionId");
+	console.log(sessionId, "sessionId");
 	if (sessionId) {
-		return await useStorage("cache").getItem<EcaasState>(sessionId);
+		const res = await useStorage("cache").getItem<EcaasState>(sessionId);
+		console.log(res, "data from cache");
+
+		return res;
 	}
 	return null;
 });
