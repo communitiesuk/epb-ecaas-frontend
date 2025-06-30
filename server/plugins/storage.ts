af0faabb94ae6fcc36f692fd676b1378afdabf70
Douglas Greenshields <douglas.greenshields@communities.gov.uk>
2025-06-30 17:55:53 +0100

    apply standard whitespace in storage plugin

--- server/plugins/storage.ts
+++ server/plugins/storage.ts
@@ -4,15 +4,15 @@ export default defineNitroPlugin(() => {
 
 	const storage = useStorage();
 
-  if (process.env.NODE_ENV === "production") {
-    const driver = redisDriver({
-      base: "{unstorage}",
-      port: 6379,
-      host: useRuntimeConfig().redisEndpoint,
-      password: useRuntimeConfig().redisPassword,
-      username: useRuntimeConfig().redisUsername,
+	if (process.env.NODE_ENV === "production") {
+		const driver = redisDriver({
+			base: "{unstorage}",
+			port: 6379,
+			host: useRuntimeConfig().redisEndpoint,
+			password: useRuntimeConfig().redisPassword,
+			username: useRuntimeConfig().redisUsername,
 			tls: {}
-    });
-    storage.mount("cache", driver);
-  }
+		});
+		storage.mount("cache", driver);
+	}
 });
