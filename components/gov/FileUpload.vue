58438ecbd9e258e64fbf284522e5bd0cc3579814
Douglas Greenshields <douglas.greenshields@communities.gov.uk>
2025-07-01 15:49:10 +0100

    EC-631 add stub import page with file upload component (not importing data yet)

---
+++ components/gov/FileUpload.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+import { createAll, FileUpload } from 'govuk-frontend';
+
+interface FileUploadProps {
+	id: string,
+	name: string,
+	label?: {
+		html: string;
+	} | {
+		text: string;
+	};
+	accept?: string;
+}
+
+const { id, label = { text: 'Upload a file' }, accept = undefined } = defineProps<FileUploadProps>();
+
+const el = useTemplateRef('el');
+
+watch(el, async (el) => {
+	console.log('el: ', el);
+	if (!el) {
+		return;
+	}
+	createAll(FileUpload, undefined, el);
+});
+
+</script>
+
+<template>
+	<div ref="el" class="govuk-form-group">
+		<!-- eslint-disable vue/no-v-html -->
+		<label v-if="'html' in label" class="govuk-label" :for="id" v-html="label.html" />
+		<!-- eslint-enable -->
+		<label v-else class="govuk-label" :for="id">
+			{{ label.text }}
+		</label>
+		<div class="govuk-drop-zone" data-module="govuk-file-upload">
+			<input :id :name type="file" class="govuk-file-upload" :accept>
+		</div>
+	</div>
+</template>
---
+++ pages/import.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const title = 'Import a calculation';
+</script>
+
+<template>
+	<Head>
+		<Title>{{ title }}</Title>
+	</Head>
+	<h1 class="govuk-heading-l">{{ title }}</h1>
+	<p class="govuk-body">To continue working on a calculation you must import a previously exported JSON file.</p>
+	<h2 class="govuk-heading-s">Upload a calculation</h2>
+	<ClientOnly>
+		<GovFileUpload id="import" name="import" accept=".json,application/json" />
+	</ClientOnly>
+	<GovWarningText>Importing a file will override any data currently in the calculation. If you wish to save this data you must first export it.</GovWarningText>
+	<GovButton href="/" secondary>Return to overview</GovButton>
+</template>
