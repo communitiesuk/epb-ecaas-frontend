4fa472f75240e3a8600db8229b395cf186254299
rachowell <rachel.howell@madetech.com>
2025-07-07 12:04:06 +0100

    EC-505 move clear data panel to clear data page
    Upon completion it seems unnecessary to have a separate component at this time.

--- pages/clear-data.vue
+++ pages/clear-data.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
+
 definePageMeta({ layout: false });
 
 const title = 'Clear data';
+
+const store = useEcaasStore();
+
+const handleClearData = async () => {
+	store.clearState();
+	await navigateTo("/");
+};
 
 </script>
 
@@ -13,6 +21,41 @@ const title = 'Clear data';
 
 		<router-link to="/" class="govuk-back-link" data-testid="backLink">Back to task list</router-link>
 
-		<ClearDataPanel />
+		<div class="govuk-panel govuk-panel--confirmation clear-data-panel" data-testid="clearDataPanel">
+			<h1 class="govuk-panel__title">
+				Are you sure you want to clear all inputted data?
+			</h1>
+			<div class="govuk-panel__body">
+				<p>This will delete all the data you've entered so far.</p>
+				<p>You can go back to undo this change.</p>
+			</div>
+			<div class="govuk-button-group">
+				<a role="button" class="govuk-button govuk-button--inverse" data-testid="clearDataButton" @click="handleClearData">Clear data</a>
+				<router-link to="/" class="govuk-link govuk-link--inverse"  data-testid="taskListLink">Go back to task list</router-link>
+			</div>
+		</div>
 	</NuxtLayout>
 </template>
+
+<style lang="scss" scoped>
+	.clear-data-panel {
+		background-color: #1d70b8;
+        text-align: left;
+        font-size: 1.1875rem;
+		margin-top: 20px;
+		padding-bottom: 25px;
+
+		h1 {
+			font-size: 2.25rem;
+		}
+
+		.govuk-button-group {
+			margin-top: 35px;
+			margin-bottom: 0;
+		}
+
+		.govuk-button {
+			margin-right: 30px;
+		}
+	}
+</style>
