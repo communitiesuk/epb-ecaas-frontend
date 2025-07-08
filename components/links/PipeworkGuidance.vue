3ac83f474cbd91eafcafff2ccd670fb5eab2c1af
kpinakula <kpinakula@users.noreply.github.com>
2025-07-08 12:23:46 +0100

    EC-695 Update pipwork guidance text

--- components/links/PipeworkGuidance.vue
+++ components/links/PipeworkGuidance.vue
@@ -1,7 +1,7 @@
 <template>
 	<p class="govuk-body">
 		<a href="/guidance/primary-and-secondary-pipework" target="_blank" class="govuk-link">
-			Primary and secondary pipework guidance (opens in another window)
+			Guidance on primary and secondary pipework (opens in another window)
 		</a>
 	</p>
 </template>
