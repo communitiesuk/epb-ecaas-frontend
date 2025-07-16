21113d8b737bb5e520028c6abd73df4300529cc7
rachowell <rachel.howell@madetech.com>
2025-07-16 11:59:53 +0100

    EC-178 update sentry client config
    Co-authored-by: Korinna Pinakoulakis <kpinakula@users.noreply.github.com>

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,12 +2,12 @@ import * as Sentry from "@sentry/nuxt";
 
 Sentry.init({
 	// If set up, you can use your runtime config here
-	dsn: useRuntimeConfig().public.sentry.dsn,
+	dsn: process.env.SENTRY_DSN_PUBLIC,
 
 	// We recommend adjusting this value in production, or using tracesSampler
 	// for finer control
 	tracesSampleRate: 1.0,
   
 	// Setting this option to true will print useful information to the console while you're setting up Sentry.
-	debug: false,
+	debug: true,
 });
