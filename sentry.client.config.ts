ad33ac456b9f1cecc1ec8acc0c8150d7ea32a2c3
kpinakula <kpinakula@users.noreply.github.com>
2025-07-17 19:48:53 +0100

    EC-178 Remove sentry tracing

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -4,10 +4,6 @@ Sentry.init({
 	// If set up, you can use your runtime config here
 	dsn: useRuntimeConfig().public.sentry.dsn,
 	environment: process.env.NODE_ENV,
-	// We recommend adjusting this value in production, or using tracesSampler
-	// for finer control
-	tracesSampleRate: 1.0,
-  
 	// Setting this option to true will print useful information to the console while you're setting up Sentry.
 	debug: false,
 });
