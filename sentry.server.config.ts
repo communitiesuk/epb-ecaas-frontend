3392ab62f447cf3ab4cb38428aa4474cb12a1ada
kpinakula <kpinakula@users.noreply.github.com>
2025-07-17 19:49:33 +0100

    EC-178 Disable sentry conosole logging

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -9,7 +9,7 @@ Sentry.init({
 	dsn: process.env.SENTRY_DSN,
 	environment: process.env.NODE_ENV,
 	// Setting this option to true will print useful information to the console while you're setting up Sentry.
-	debug: true,
+	debug: false,
 });
 
 console.log('--- Sentry.init() call executed ---');
