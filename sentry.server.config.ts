b83b7304a1b32144086cb5ef24b90c36c83d0bd7
rachowell <rachel.howell@madetech.com>
2025-07-18 09:57:23 +0100

    EC-178 remove sentry console logs

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -3,13 +3,9 @@ import dotenv from "dotenv";
  
 dotenv.config();
 
-console.log('--- Sentry.server.config.mjs file loaded/parsed ---');
-
 Sentry.init({
 	dsn: process.env.SENTRY_DSN,
 	environment: process.env.NODE_ENV,
 	// Setting this option to true will print useful information to the console while you're setting up Sentry.
 	debug: false,
 });
-
-console.log('--- Sentry.init() call executed ---');
