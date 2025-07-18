7a8e6f229d866d3f4a1a2a82482ca8f757706cd3
rachowell <rachel.howell@madetech.com>
2025-07-18 10:09:39 +0100

    EC-178 update environment in sentry server config

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,7 +5,6 @@ dotenv.config();
 
 Sentry.init({
 	dsn: process.env.SENTRY_DSN,
-	environment: process.env.NODE_ENV,
-	// Setting this option to true will print useful information to the console while you're setting up Sentry.
+	environment: process.env.SENTRY_ENVIRONMENT || process.env.NODE_ENV,
 	debug: false,
 });
