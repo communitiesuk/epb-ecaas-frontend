6cbc6fb5d9645eeb94497b8c3ad6761e52525ebe
rachowell <rachel.howell@madetech.com>
2025-07-21 11:26:22 +0100

    EC-178 remove sentry client setup
    Co-authored-by: Korinna Pinakoulakis <kpinakula@users.noreply.github.com>

--- sentry.client.config.ts
+++
@@ -1,7 +0,0 @@
-import * as Sentry from "@sentry/nuxt";
-
-Sentry.init({
-	dsn: useRuntimeConfig().public.sentry.dsn,
-	environment: useRuntimeConfig().public.sentry.environment,
-	debug: false,
-});
