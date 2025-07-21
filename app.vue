acc2d33935489b7b73afd134ca7cabe5dcac00e2
James Wainwright <james.wainwright@madetech.com>
2025-07-21 10:53:00 +0100

    EC-765 - Beta banner with feedback link

--- app.vue
+++ app.vue
@@ -2,8 +2,8 @@
 	<GovHeader classes="govuk-header--full-width-border" />
 	<GovServiceNavigation service-name="Check Part L building compliance" />
 	<div class="govuk-width-container">
-		<GovPhaseBanner tag="Alpha">
-			This is a new service - your feedback will help us to improve it.
+		<GovPhaseBanner tag="BETA">
+			This is a new service. Help us to improve it and <NuxtLink href="/feedback" class="govuk-link">give your feedback</NuxtLink>.
 		</GovPhaseBanner>
 		<main class="govuk-main-wrapper govuk-!-padding-top-6">
 			<div class="govuk-grid-row">
