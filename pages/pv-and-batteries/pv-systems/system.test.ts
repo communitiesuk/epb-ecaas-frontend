43b747052a22e02aaa090d6b7b05004a755091c1
James Wainwright <james.wainwright@madetech.com>
2025-07-25 16:00:21 +0100

    EC-746 - Resolve failing test

--- pages/pv-and-batteries/pv-systems/system.test.ts
+++ pages/pv-and-batteries/pv-systems/system.test.ts
@@ -75,7 +75,7 @@ describe("PV system", () => {
 		expect(screen.getByText("Inverter peak power AC")).toBeDefined();
 		expect(screen.getByText("Inverter peak power DC")).toBeDefined();
 		expect(screen.getByText("Location of inverter")).toBeDefined();
-		expect(screen.getByText("Inverter type")).toBeDefined();
+		expect(screen.getByText("Inverter type", { selector: 'legend' })).toBeDefined();
 		expect(screen.queryByText("PV shading")).toBeNull();
 	});
 
