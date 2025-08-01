81a2420fba3c71038eb61db73be8a83042be970f
Harriet Hall <harriet.hall@madetech.com>
2025-08-01 16:20:11 +0100

    update playwright buildspec - add

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,17 +17,18 @@ dotenv.config({ path: path.resolve(__dirname, '.env') });
  */
 
 export default defineConfig({
+	
 	testDir: './e2e-tests',
 	/* Run tests in files in parallel */
-	fullyParallel: true,
+	fullyParallel: false,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
-	forbidOnly: !!process.env.CI,
+	// forbidOnly: !!process.env.CI,
 	/* Retry on CI only */
 	retries: 3,
 	/* Opt out of parallel tests on CI. */
-	workers: process.env.CI ? 1 : undefined,
+	// workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	// reporter: 'html',
+	reporter: 'html',
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
@@ -90,9 +91,10 @@ export default defineConfig({
 	],
 
 	/* Run your local dev server before starting the tests */
-	// webServer: {
-	//   command: 'npm run start',
-	//   url: 'http://localhost:3000',
-	//   reuseExistingServer: !process.env.CI,
-	// },
+	webServer: {
+	  command: 'npm run dev',
+	  url: 'http://localhost:3000',
+	  reuseExistingServer: !process.env.CI,
+		  timeout: 120 * 1000
+	},
 });
