8c3670fec0c29f1624d88955b8df72f3cf8b58d3
kpinakula <kpinakula@users.noreply.github.com>
2025-08-04 12:01:31 +0100

    Update description of design temp across emitters
    This commit updates the descritption according to the feedback from
    DESNZ.
    
    Co-Authored-By: Rachel Howell <98879818+rachowell@users.noreply.github.com>

--- pages/heating-systems/heat-emitting/wet-distribution/[distribution].vue
+++ pages/heating-systems/heat-emitting/wet-distribution/[distribution].vue
@@ -136,7 +136,7 @@ const { handleInvalidSubmit, errorMessages } = useErrorSummary();
 			name="designTempDiffAcrossEmitters"
 			validation="required | number"
 			suffix-text="°C"
-			help="Enter the difference between the water flow temperature and the room air temperature at the point where the heating is being emitted. Typically between 5 and 15 °C."
+			help="The temperature difference between the flow and return water temperatures at each emitter. Typically between 5 and 15°C."
 		/>
 			
 		<FormKit
