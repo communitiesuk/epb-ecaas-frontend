bf72564f62309bdfad7ffc6dea5c541c360c83e2
Harriet Hall <harriet.hall@madetech.com>
2025-08-04 12:59:34 +0100

    only trigger plugins which set/get data from remote db when the node env is not prod

--- plugins/update-cache.client.ts
+++ plugins/update-cache.client.ts
@@ -1,7 +1,8 @@
 export default defineNuxtPlugin(() => {
-	const store = useEcaasStore();
-
-	store.$subscribe(async () => {
-		await store.postEcaasState();
-	});
+	if(process.env.NODE_ENV !== "production"){
+		const store = useEcaasStore();
+		store.$subscribe(async () => {
+			await store.postEcaasState();
+		});
+	}
 });
