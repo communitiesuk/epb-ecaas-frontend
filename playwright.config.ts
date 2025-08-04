729b79f110d1974e98f87c9213704331125f048b
Harriet Hall <harriet.hall@madetech.com>
2025-08-04 11:25:29 +0100

    update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
 	
 	testDir: './e2e-tests',
 	/* Run tests in files in parallel */
-	fullyParallel: false,
+	fullyParallel: true,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
 	// forbidOnly: !!process.env.CI,
 	/* Retry on CI only */
@@ -28,7 +28,7 @@ export default defineConfig({
 	/* Opt out of parallel tests on CI. */
 	// workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: 'html',
+	// reporter: 'html',
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
@@ -91,10 +91,11 @@ export default defineConfig({
 	],
 
 	/* Run your local dev server before starting the tests */
-	// webServer: {
+	
+	// webServer: !process.env.BASE_URL ? {
 	//   command: 'npm run dev',
 	//   url: 'http://localhost:3000',
 	//   reuseExistingServer: !process.env.CI,
 	// 	  timeout: 120 * 1000
-	// },
+	// } : undefined,
 });
