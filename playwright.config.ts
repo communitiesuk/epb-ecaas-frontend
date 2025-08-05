55b270488a8be9af86d9ff7671053731ee29b83f
Harriet Hall <harriet.hall@madetech.com>
2025-08-05 14:48:54 +0100

    format playwright.config

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,111 +17,111 @@ dotenv.config({ path: path.resolve(__dirname, ".env") });
  */
 
 export default defineConfig({
-  testDir: "./e2e-tests",
-  /* Run tests in files in parallel */
-  fullyParallel: process.env.BASE_URL ? true : false,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  // forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: 3,
-  /* Opt out of parallel tests on CI. */
-  // workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  // reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.BASE_URL || "http://localhost:3000",
-    launchOptions: {
-      slowMo: 1000,
-    },
+	testDir: "./e2e-tests",
+	/* Run tests in files in parallel */
+	fullyParallel: process.env.BASE_URL ? true : false,
+	/* Fail the build on CI if you accidentally left test.only in the source code. */
+	// forbidOnly: !!process.env.CI,
+	/* Retry on CI only */
+	retries: 3,
+	/* Opt out of parallel tests on CI. */
+	// workers: process.env.CI ? 1 : undefined,
+	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
+	// reporter: 'html',
+	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+	use: {
+		/* Base URL to use in actions like `await page.goto('/')`. */
+		baseURL: process.env.BASE_URL || "http://localhost:3000",
+		launchOptions: {
+			slowMo: 1000,
+		},
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "off",
-  },
-  /* Configure projects for major browsers */
-  // Disabled Webkit when running app in localhost - Webkit sends network requests over https not http (eg http://localhost:3000)
-  projects: process.env.BASE_URL
-    ? [
-        { name: "setup", testMatch: /.*\.setup\.ts/ },
-        {
-          name: "chromium",
-          use: {
-            ...devices["Desktop Chrome"],
-            // Use prepared auth state.
-            storageState: "playwright/.auth/user.json",
-          },
-          dependencies: ["setup"],
-        },
-        {
-          name: "firefox",
-          use: {
-            ...devices["Desktop Firefox"],
-            // Use prepared auth state.
-            storageState: "playwright/.auth/user.json",
-          },
-          dependencies: ["setup"],
-        },
-        {
-          name: "webkit",
-          use: {
-            ...devices["Desktop Safari"],
-            // Use prepared auth state.
-            storageState: "playwright/.auth/user.json",
-          },
-          dependencies: ["setup"],
-        },
-      ]
-    : [
-        { name: "setup", testMatch: /.*\.setup\.ts/ },
-        {
-          name: "chromium",
-          use: {
-            ...devices["Desktop Chrome"],
-            // Use prepared auth state.
-            storageState: "playwright/.auth/user.json",
-          },
-          dependencies: ["setup"],
-        },
-        {
-          name: "firefox",
-          use: {
-            ...devices["Desktop Firefox"],
-            // Use prepared auth state.
-            storageState: "playwright/.auth/user.json",
-          },
-          dependencies: ["setup"],
-        },
-      ],
+		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+		trace: "off",
+	},
+	/* Configure projects for major browsers */
+	// Disabled Webkit when running app in localhost - Webkit sends network requests over https not http (eg http://localhost:3000)
+	projects: process.env.BASE_URL
+		? [
+			{ name: "setup", testMatch: /.*\.setup\.ts/ },
+			{
+				name: "chromium",
+				use: {
+					...devices["Desktop Chrome"],
+					// Use prepared auth state.
+					storageState: "playwright/.auth/user.json",
+				},
+				dependencies: ["setup"],
+			},
+			{
+				name: "firefox",
+				use: {
+					...devices["Desktop Firefox"],
+					// Use prepared auth state.
+					storageState: "playwright/.auth/user.json",
+				},
+				dependencies: ["setup"],
+			},
+			{
+				name: "webkit",
+				use: {
+					...devices["Desktop Safari"],
+					// Use prepared auth state.
+					storageState: "playwright/.auth/user.json",
+				},
+				dependencies: ["setup"],
+			},
+		]
+		: [
+			{ name: "setup", testMatch: /.*\.setup\.ts/ },
+			{
+				name: "chromium",
+				use: {
+					...devices["Desktop Chrome"],
+					// Use prepared auth state.
+					storageState: "playwright/.auth/user.json",
+				},
+				dependencies: ["setup"],
+			},
+			{
+				name: "firefox",
+				use: {
+					...devices["Desktop Firefox"],
+					// Use prepared auth state.
+					storageState: "playwright/.auth/user.json",
+				},
+				dependencies: ["setup"],
+			},
+		],
 
-  /* Test against mobile viewports. */
-  // {
-  //   name: 'Mobile Chrome',
-  //   use: { ...devices['Pixel 5'] },
-  // },
-  // {
-  //   name: 'Mobile Safari',
-  //   use: { ...devices['iPhone 12'] },
-  // },
+	/* Test against mobile viewports. */
+	// {
+	//   name: 'Mobile Chrome',
+	//   use: { ...devices['Pixel 5'] },
+	// },
+	// {
+	//   name: 'Mobile Safari',
+	//   use: { ...devices['iPhone 12'] },
+	// },
 
-  /* Test against branded browsers. */
-  // {
-  //   name: 'Microsoft Edge',
-  //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-  // },
-  // {
-  //   name: 'Google Chrome',
-  //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-  // },
+	/* Test against branded browsers. */
+	// {
+	//   name: 'Microsoft Edge',
+	//   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+	// },
+	// {
+	//   name: 'Google Chrome',
+	//   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+	// },
 
-  /* Run your local dev server before starting the tests */
+	/* Run your local dev server before starting the tests */
 
-  webServer: !process.env.BASE_URL
-    ? {
-        command: "npm run dev",
-        url: "http://localhost:3000",
-        reuseExistingServer: !process.env.CI,
-        timeout: 120 * 1000,
-      }
-    : undefined,
+	webServer: !process.env.BASE_URL
+		? {
+			command: "npm run dev",
+			url: "http://localhost:3000",
+			reuseExistingServer: !process.env.CI,
+			timeout: 120 * 1000,
+		}
+		: undefined,
 });
