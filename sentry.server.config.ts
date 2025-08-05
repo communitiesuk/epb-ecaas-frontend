8a6dbf534a26ec9a0edb690c4cfd3ccfd75b3c4a
rachowell <rachel.howell@madetech.com>
2025-08-05 12:46:16 +0100

    EC-854 add comment to sentry config with guidance for enabling logging in dev mode
    This will ensure the correct environment shows in Sentry.

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,6 +1,9 @@
 import * as Sentry from "@sentry/nuxt";
 import dotenv from "dotenv";
- 
+
+// This project is configured to not log to Sentry for development.
+// To temporarily enable Sentry for development, use this command to start the server:
+// NUXT_PUBLIC_ENVIRONMENT='development' npm run dev
 if (process.env.NUXT_PUBLIC_ENVIRONMENT) {
 	dotenv.config();
 	Sentry.init({
