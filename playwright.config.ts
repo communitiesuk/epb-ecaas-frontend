75dc981147a330924deb4cf595f08f1b581fe396
Harriet Hall <harriet.hall@madetech.com>
2025-08-05 09:21:49 +0100

    update playwright config so e2e tests run sequentially ( in dev), & in slowMo

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
 	
 	testDir: './e2e-tests',
 	/* Run tests in files in parallel */
-	fullyParallel: true,
+	fullyParallel: process.env.BASE_URL ? true : false,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
 	// forbidOnly: !!process.env.CI,
 	/* Retry on CI only */
@@ -33,6 +33,10 @@ export default defineConfig({
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
 		baseURL: process.env.BASE_URL || "http://localhost:3000",
+		launchOptions:{
+			slowMo: 1000,
+		},
+
     
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
