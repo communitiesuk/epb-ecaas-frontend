e998a0da9a97f51b7232ef09a00084698c1e1f0b
Harriet Hall <harriet.hall@madetech.com>
2025-09-10 13:22:22 +0100

    EC-912: Windows summary page -partically saving data

--- pages/dwelling-space/summary.test.ts
+++ pages/dwelling-space/summary.test.ts
@@ -213,30 +213,33 @@ const doorsData: DoorsData = {
 	}
 };
 
-const windowData: WindowData = {
-	name: "Window 1",
-	orientation: 1,
-	surfaceArea: 1,
-	height: 1,
-	width: 1,
-	uValue: 1,
-	pitchOption: "90",
-	pitch: 90,
-	solarTransmittance: 0.1,
-	elevationalHeight: 1,
-	midHeight: 1,
-	openingToFrameRatio: 0.2,
-	numberOpenableParts: "0",
-	overhangDepth: unitValue(100, millimetre),
-	overhangDistance: unitValue(100, millimetre),
-	sideFinRightDepth: unitValue(100, millimetre),
-	sideFinRightDistance: unitValue(100, millimetre),
-	sideFinLeftDepth: unitValue(100, millimetre),
-	sideFinLeftDistance: unitValue(100, millimetre),
-	curtainsOrBlinds: true,
-	treatmentType: WindowTreatmentType.blinds,
-	thermalResistivityIncrease: 1,
-	solarTransmittanceReduction: 0.1,
+const windowData: EcaasForm<WindowData> = {
+	data: {
+		name: "Window 1",
+		orientation: 1,
+		surfaceArea: 1,
+		height: 1,
+		width: 1,
+		uValue: 1,
+		pitchOption: "90",
+		pitch: 90,
+		solarTransmittance: 0.1,
+		elevationalHeight: 1,
+		midHeight: 1,
+		openingToFrameRatio: 0.2,
+		numberOpenableParts: "0",
+		overhangDepth: unitValue(100, millimetre),
+		overhangDistance: unitValue(100, millimetre),
+		sideFinRightDepth: unitValue(100, millimetre),
+		sideFinRightDistance: unitValue(100, millimetre),
+		sideFinLeftDepth: unitValue(100, millimetre),
+		sideFinLeftDistance: unitValue(100, millimetre),
+		curtainsOrBlinds: true,
+		treatmentType: WindowTreatmentType.blinds,
+		thermalResistivityIncrease: 1,
+		solarTransmittanceReduction: 0.1,
+
+	}
 };
 
 const thermalBridgingData: ThermalBridgingData = {
@@ -737,7 +740,8 @@ describe("Living space fabric summary", () => {
 			store.$patch({
 				dwellingFabric: {
 					dwellingSpaceWindows: {
-						data: [windowData]
+						data: [windowData],
+							complete: true
 					}
 				}
 			});
