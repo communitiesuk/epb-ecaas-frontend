4112d338c344770026e0c98f482fbb2074fc45e3
James Wainwright <james.wainwright@madetech.com>
2025-09-18 13:09:38 +0100

    EC-931 - Backwards compatibility for data in old format

--- stores/revalidate.test.ts
+++ stores/revalidate.test.ts
@@ -69,6 +69,46 @@ const twoCompleteValidWalls: Pick<WallsData, "dwellingSpaceExternalWall"> = {
 	},
 };
 
+const twoLegacyCompleteValidWalls = {
+	"dwellingFabric": {
+		"dwellingSpaceWalls": {
+			"dwellingSpaceExternalWall": {
+				"data": [
+					{
+						"name": "Wall 1",
+						"pitchOption": "90",
+						"pitch": 90,
+						"orientation": 140,
+						"height": 6,
+						"length": 5,
+						"elevationalHeight": 25,
+						"surfaceArea": 40,
+						"solarAbsorption": 0.9,
+						"uValue": 5,
+						"kappaValue": 110000,
+						"massDistributionClass": MassDistributionClass.E,
+					},
+					{
+						"name": "Wall 2",
+						"pitchOption": "90",
+						"pitch": 90,
+						"orientation": 90,
+						"height": 30,
+						"length": 34,
+						"elevationalHeight": 20,
+						"surfaceArea": 30,
+						"solarAbsorption": 0.7,
+						"uValue": 6,
+						"kappaValue": 175000,
+						"massDistributionClass": MassDistributionClass.I,
+					},
+				],
+				"complete": true,
+			},
+		},
+	},
+};
+
 const twoWallsOneMissingField = immutable.del(twoCompleteValidWalls, "dwellingSpaceExternalWall.data.1.data.solarAbsorption");
 
 const twoWallsOneMissingFieldOneInvalidValue = immutable.set(twoWallsOneMissingField, "dwellingSpaceExternalWall.data.0.massDistributionClass", "X"); // 'X' is invalid for a mass distribution class value
@@ -197,7 +237,7 @@ const cases: [string, Record<string, unknown>, boolean, Record<string, unknown> 
 				"dwellingSpaceWalls": immutable.set(twoWallsOneMissingFieldOneInvalidValue, "dwellingSpaceExternalWall.complete", false),
 			},
 		},
-		2,
+		1,
 	],
 	[
 		"case where there are two heat pumps (lists of items each with complete states), all complete and valid",
@@ -212,6 +252,17 @@ const cases: [string, Record<string, unknown>, boolean, Record<string, unknown> 
 		true,
 		immutable.set(immutable.set(twoHeatPumpsOneWithMissingFields, "heatingSystems.heatGeneration.heatPump.data.1.complete", false), "heatingSystems.heatGeneration.heatPump.complete", false),
 		1,
+	],
+	[
+		"case where there is a legacy array of form data: data should be wrapped in EcaasForm objects",
+		twoLegacyCompleteValidWalls,
+		false,
+		{
+			"dwellingFabric": {
+				"dwellingSpaceWalls": twoCompleteValidWalls,
+			},
+		},
+		0,
 	],
 ];
 
--- stores/revalidate.ts
+++ stores/revalidate.ts
@@ -25,12 +25,15 @@ export function revalidateState(state: Record<string, unknown>, path: string[] =
 			if (!value.complete) {
 				continue;
 			}
+
 			const [formIsInvalid, formErrors] = revalidateForm(value, formPath(path, key));
+
 			if (formIsInvalid) {
 				errors.push(...formErrors);
 			}
 		} else if (typeof value === "object" && value !== null) {
 			const nodeResult = revalidateState(value as Record<string, unknown>, [...path, key]);
+			
 			if (nodeResult.changed) {
 				errors.push(...nodeResult.errors);
 				newState[key] = nodeResult.newState;
@@ -43,13 +46,13 @@ export function revalidateState(state: Record<string, unknown>, path: string[] =
 			newState: state,
 			changed: false,
 		};
-	} else {
-		return {
-			newState,
-			changed: true,
-			errors,
-		};
 	}
+
+	return {
+		newState,
+		changed: true,
+		errors,
+	};
 }
 
 function formPath(path: string[], currentKey: string): EcaasFormPath {
@@ -58,50 +61,64 @@ function formPath(path: string[], currentKey: string): EcaasFormPath {
 
 function revalidateForm(form: EcaasForm<unknown>, path: EcaasFormPath): [true, ZodError<unknown>[]] | [false] {
 	const formSchema = formSchemas[path];
+
 	// following could happen if state contains nodes no longer in the schema, but if so we should just ignore these nodes
 	if (!formSchema) {
 		return [false];
 	}
-	const formData = form.data;
-	if (!Array.isArray(formData)) {
-		const validationResult = formSchema.safeParse(formData);
+
+	// Form data is a single data object
+	if (!Array.isArray(form.data)) {
+		const validationResult = formSchema.safeParse(form.data);
+
 		if (validationResult.success) {
 			return [false];
-		} else {
-			console.log(`revalidation found invalid form at path ${path}:`, validationResult.error);
-			form.complete = false;
-			return [true, [validationResult.error]];
 		}
-	} else if (formData.every(isEcaasForm)) {
-		// it could be an array of sub-forms
-		const [changed, errors]: [boolean, ZodError<unknown>[]] = formData.reduce(([changed, errors], current) => {
-			const validationResult = formSchema.safeParse(current.data);
-			if (!validationResult.success) {
-				current.complete = false;
-			}
-            
-			return [!validationResult.success || changed, validationResult.error ? [...errors, validationResult.error] : errors];
-		}, [false as boolean, [] as ZodError<unknown>[]]);
 
-		if (changed) {
-			form.complete = false;
-			return [changed, errors];
-		} else {
-			return [false];
-		}
-	} else {
-		// it could also be an array of data
-		const [changed, errors]: [boolean, ZodError<unknown>[]] = formData.reduce(([changed, errors], current) => {
-			const validationResult = formSchema.safeParse(current);
-            
-			return [!validationResult.success || changed, validationResult.error ? [...errors, validationResult.error] : errors];
-		}, [false, []]);
+		console.log(`revalidation found invalid form at path ${path}:`, validationResult.error);
+		form.complete = false;
 
-		if (changed) {
-			form.complete = false;
-			return [changed, errors];
-		} else {
-			return [false];
-		}
+		return [true, [validationResult.error]];
 	}
+
+	// Form data is an array
+	const allEcaasForms = form.data.every(isEcaasForm);
+	let ecaasFormData: EcaasForm<unknown>[] = allEcaasForms ? form.data : [];
+
+	// Check for any non-EcaasForm objects in array
+	if (!allEcaasForms) {
+		
+		// Extract form data which are EcaasForms
+		const ecaasForms = form.data.filter(isEcaasForm);
+
+		// Extract form data which are not EcaasForms
+		const nonEcaasForms = form.data.filter(x => !isEcaasForm(x));
+
+		// Wrap form data in an array of EcaasForms and join with EcaasForms
+		ecaasFormData = ecaasForms.concat(nonEcaasForms.map(x => ({
+			data: x,
+			complete: true,
+		})));
+
+		// Update form data reference
+		form.data = ecaasFormData;
+	}
+
+	// Validate data in each EcaasForm
+	const [changed, errors]: [boolean, ZodError<unknown>[]] = ecaasFormData.reduce(([changed, errors], current) => {
+		const validationResult = formSchema.safeParse(current.data);
+
+		if (!validationResult.success) {
+			current.complete = false;
+		}
+		
+		return [!validationResult.success || changed, validationResult.error ? [...errors, validationResult.error] : errors];
+	}, [false as boolean, [] as ZodError<unknown>[]]);
+
+	if (changed) {
+		form.complete = false;
+		return [changed, errors];
+	}
+
+	return [false];
 }
