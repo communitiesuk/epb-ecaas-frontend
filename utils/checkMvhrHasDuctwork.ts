77f431175a2685bee1811e775bf83c9d9bb2e928
Douglas Greenshields <douglas.greenshields@communities.gov.uk>
2025-09-19 13:13:07 +0100

    add defensive check for undefined data in core function

--- utils/checkMvhrHasDuctwork.ts
+++ utils/checkMvhrHasDuctwork.ts
@@ -3,7 +3,7 @@ import { VentType } from "~/schema/api-schema.types";
 export const checkMvhrHasDuctwork = () => {
 	const store = useEcaasStore();
 
-	let mvhrArray = store.infiltrationAndVentilation.mechanicalVentilation.data.filter(x => x.data.typeOfMechanicalVentilationOptions === VentType.MVHR);
+	let mvhrArray = store.infiltrationAndVentilation.mechanicalVentilation.data.filter(x => x.data?.typeOfMechanicalVentilationOptions === VentType.MVHR);
 
 	const ductworkArray = store.infiltrationAndVentilation.ductwork.data.map(x => x.data.mvhrUnit);
 	const uniqueDuctworkArray = [...new Set(ductworkArray)];
