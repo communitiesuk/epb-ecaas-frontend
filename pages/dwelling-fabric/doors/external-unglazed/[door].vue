c1adab30b61062afe12483490d90f6ff2d9fbde3
Adil Rahman <232363244+adil-rahman-mt@users.noreply.github.com>
2025-09-23 11:10:40 +0100

    Add pitch data to store during auto save in all 3 door forms

--- pages/dwelling-fabric/doors/external-unglazed/[door].vue
+++ pages/dwelling-fabric/doors/external-unglazed/[door].vue
@@ -40,6 +40,8 @@ autoSaveElementForm<ExternalUnglazedDoorData>({
 	storeData: store.dwellingFabric.dwellingSpaceDoors.dwellingSpaceExternalUnglazedDoor,
 	defaultName: "External unglazed door",
 	onPatch: (state, newData, index) => {
+		const { pitchOption, pitch } = newData.data;
+		newData.data.pitch = pitchOption === "90" ? 90 : pitch;
 		state.dwellingFabric.dwellingSpaceDoors.dwellingSpaceExternalUnglazedDoor.data[index] = newData;
 		state.dwellingFabric.dwellingSpaceDoors.dwellingSpaceExternalUnglazedDoor.complete = false;
 	},
