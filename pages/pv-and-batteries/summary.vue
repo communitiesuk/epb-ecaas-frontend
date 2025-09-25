89c91754265202857689b20709bfd11345b271cf
kpinakula <kpinakula@users.noreply.github.com>
2025-09-25 14:35:59 +0100

    EC-927 Refactor unit pv & batteries summary

--- pages/pv-and-batteries/summary.vue
+++ pages/pv-and-batteries/summary.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
 import type { SummarySection } from "~/common.types";
 import { getTabItems, getUrl } from "#imports";
-import { kilowatt, kilowattHour, kilowattPeak } from "~/utils/units/power";
-import { degrees } from "~/utils/units/angle";
-import { metre } from "~/utils/units/length";
 
 const title = "PV and electric batteries summary";
 const store = useEcaasStore();
@@ -15,15 +12,15 @@ const pvSummary: SummarySection = {
 	data: pvSystems.map(({ data: x }) => {
 		return {
 			"Name": x.name,
-			"Peak power": dim(x.peakPower, kilowattPeak.name),
+			"Peak power": dim(x.peakPower, "kilowatt peak"),
 			"Ventilation strategy": displaySnakeToSentenceCase(show(x.ventilationStrategy)),
-			"Pitch": dim(x.pitch, degrees.name),
-			"Orientation": dim(x.orientation, degrees.name),
-			"Elevational height of PV": dim(x.elevationalHeight, metre.name),
-			"Length of PV": dim(x.lengthOfPV, metre.name),
-			"Width of PV": dim(x.widthOfPV, metre.name),
-			"Inverter peak power AC": dim(x.inverterPeakPowerAC, kilowatt.name),
-			"Inverter peak power DC": dim(x.inverterPeakPowerDC, kilowatt.name),
+			"Pitch": dim(x.pitch, "degrees"),
+			"Orientation": dim(x.orientation, "degrees"),
+			"Elevational height of PV": dim(x.elevationalHeight, "metres"),
+			"Length of PV": dim(x.lengthOfPV, "metres"),
+			"Width of PV": dim(x.widthOfPV, "metres"),
+			"Inverter peak power AC": dim(x.inverterPeakPowerAC, "kilowatt"),
+			"Inverter peak power DC": dim(x.inverterPeakPowerDC, "kilowatt"),
 			"Inverter is inside": displayBoolean(x.inverterIsInside),
 			"Inverter type": displaySnakeToSentenceCase(show(x.inverterType)),
 		};
@@ -38,14 +35,14 @@ const batterySummary: SummarySection = {
 	data: electricBattery.map(({ data: x }) => {
 		return {
 			"Name": x.name,
-			"Capacity": dim(x.capacity, kilowattHour.name),
+			"Capacity": dim(x.capacity, "kilowatt-hour"),
 			"Battery age": x.batteryAge ? `${show(x.batteryAge)} years` : show(x.batteryAge),
 			"Charge efficiency": show(x.chargeEfficiency),
 			"Location": displayCamelToSentenceCase(show(x.location)),
 			"Grid charging possible": displayBoolean(x.gridChargingPossible),
-			"Maximum charge rate": dim(x.maximumChargeRate, kilowatt.name),
-			"Minimum charge rate": dim(x.minimumChargeRate, kilowatt.name),
-			"Maximum discharge rate": dim(x.maximumDischargeRate, kilowatt.name),
+			"Maximum charge rate": dim(x.maximumChargeRate, "kilowatt"),
+			"Minimum charge rate": dim(x.minimumChargeRate, "kilowatt"),
+			"Maximum discharge rate": dim(x.maximumDischargeRate, "kilowatt"),
 		};
 	}),
 	editUrl: "/pv-and-batteries",
