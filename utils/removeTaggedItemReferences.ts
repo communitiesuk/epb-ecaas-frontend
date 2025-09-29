933704e3c415ca98a1e5c298f49495e933af8c98
Harriet Hall <harriet.hall@madetech.com>
2025-09-29 11:41:14 +0100

    update parameter names for clarity

--- utils/removeTaggedItemReferences.ts
+++ utils/removeTaggedItemReferences.ts
@@ -1,15 +1,15 @@
 export const removeTaggedItemReferences = <T extends Record<string, unknown>>(
   storeItems: EcaasFormList<T>,
-  id: string | undefined,
-  taggedValue: keyof T
+  taggedItemId: string | undefined,
+  keyToCheck: keyof T
 ) => {
   const store = useEcaasStore();
 
   storeItems.data.forEach((x, index) => {
     const data = x.data as Partial<T>;
-    if (data[taggedValue] === id) {
+    if (data[keyToCheck] === taggedItemId) {
       store.$patch(() => {
-        (storeItems.data[index]!.data as Partial<T>)[taggedValue] = undefined;
+        (storeItems.data[index]!.data as Partial<T>)[keyToCheck] = undefined;
         storeItems.data[index]!.complete = false;
         storeItems.complete = false;
       });
