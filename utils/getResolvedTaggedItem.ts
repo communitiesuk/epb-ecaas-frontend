6e46cabfa3c76e44d16eeb8dadca77bdfad228b8
Harriet Hall <harriet.hall@madetech.com>
2025-10-07 09:23:00 +0100

    rename utils file to getRestolvedTaggedItems

---
+++ utils/getResolvedTaggedItem.ts
@@ -0,0 +1,65 @@
+type EcaasSectionWithTagging =
+  | WindowData
+  | ExternalWallData
+  | InternalWallData
+  | RoofData
+  | CeilingData;
+
+export const getResolvedTaggedItem = (
+  sections: EcaasSectionWithTagging[][],
+  id: string
+): AssociatedItemValues | undefined => {
+  const topLevelTaggedItem = getResolvedTopLevelTaggedItem(sections, id);
+  if (topLevelTaggedItem) return topLevelTaggedItem;
+
+  const nestedTaggedItem = getResolvedNestedTaggedItem(sections, id);
+  if (nestedTaggedItem && "taggedItem" in nestedTaggedItem) {
+    return getResolvedTaggedItem(sections, nestedTaggedItem.taggedItem);
+  }
+};
+
+export const extractResolvedSectionItems = <T extends Record<string, unknown>>(
+  section: T[]
+): AssociatedItemValues[] => {
+  return (
+    section?.map((x) => {
+      return {
+        id: x.id!,
+        pitch: x.pitchOption === undefined ? x.pitch : extractPitch(x),
+        ...("orientation" in x && { orientation: x.orientation }),
+      } as AssociatedItemValues;
+    }) ?? []
+  );
+};
+
+export const getResolvedTopLevelTaggedItem = (
+  sections: EcaasSectionWithTagging[][],
+  id: string | undefined
+): AssociatedItemValues | undefined => {
+  const items: AssociatedItemValues[][] = [];
+  const sectionsWithoutNestedTaggedItems = sections?.filter(
+    (s) => s !== undefined && s.some((x) => !("taggedItem" in x))
+  );
+
+  for (const section of sectionsWithoutNestedTaggedItems) {
+    items.push(extractResolvedSectionItems(section));
+  }
+  const taggedItem = items.flat().find((item) => item.id === id);
+  return taggedItem;
+};
+
+export const getResolvedNestedTaggedItem = (
+  sections: EcaasSectionWithTagging[][],
+  id: string | undefined
+) => {
+  const sectionsWithNestedTaggedItems = sections.filter(
+    (s) => s !== undefined && s.some((x) => "taggedItem" in x)
+  );
+  for (const section of sectionsWithNestedTaggedItems) {
+    const taggedItem = section.find((x) => "id" in x && x.id === id);
+
+    if (taggedItem) {
+      return taggedItem;
+    }
+  }
+};
