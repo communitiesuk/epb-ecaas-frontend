14d91456e0458ec5ed702e8fa3397c1123e930d8
Douglas Greenshields <douglas.greenshields@communities.gov.uk>
2025-10-15 17:07:13 +0100

    fix import type for domestic hot water mapper

--- mapping/domesticHotWaterMapper.ts
+++ mapping/domesticHotWaterMapper.ts
@@ -1,5 +1,5 @@
-import type { SchemaBathDetails, SchemaHotWaterSourceDetails, SchemaOtherWaterUseDetails, SchemaShower } from "~/schema/aliases";
-import type { SchemaStorageTank, SchemaWaterPipework } from "~/schema/api-schema.types";
+import type { SchemaBathDetails, SchemaHotWaterSourceDetails, SchemaOtherWaterUseDetails, SchemaShower, SchemaWaterPipework } from "~/schema/aliases";
+import type { SchemaStorageTank } from "~/schema/api-schema.types";
 import type { FhsInputSchema, ResolvedState } from "./fhsInputMapper";
 import { defaultControlMaxName, defaultControlMinName, defaultElectricityEnergySupplyName } from "./common";
 import { asLitres } from "../utils/units/volume";
