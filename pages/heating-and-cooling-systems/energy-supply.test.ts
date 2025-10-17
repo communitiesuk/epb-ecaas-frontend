52146ab2b9e4172e7d160e5420b6b48fe5149c2e
Adil Rahman <232363244+adil-rahman-mt@users.noreply.github.com>
2025-10-17 11:10:53 +0100

    Remove redundant import

--- pages/heating-and-cooling-systems/energy-supply.test.ts
+++ pages/heating-and-cooling-systems/energy-supply.test.ts
@@ -2,7 +2,6 @@ import EnergySupply from "./energy-supply.vue";
 import { screen } from "@testing-library/vue";
 import { mockNuxtImport, renderSuspended } from "@nuxt/test-utils/runtime";
 import { userEvent } from "@testing-library/user-event";
-import type { EnergySupplyData } from "~/stores/ecaasStore.schema";
 
 const navigateToMock = vi.hoisted(() => vi.fn());
 mockNuxtImport("navigateTo", () => {
