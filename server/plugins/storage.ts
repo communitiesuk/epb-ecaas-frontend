a94984b1b74db4ed409aea1b5955087148d517e5
kpinakula <kpinakula@users.noreply.github.com>
2025-10-23 12:40:16 +0100

    EC-999 Replace Redis with custom DynamoDB driver
    Co-Authored-By: Harriet Hall <161617232+Harriet-Hall@users.noreply.github.com>

--- server/plugins/storage.ts
+++ server/plugins/storage.ts
@@ -1,18 +1,37 @@
-import redisDriver from "unstorage/drivers/redis";
+import { defineDriver } from "unstorage";
+import {
+	getItem as getSession,
+	setItem as setSession,
+} from "../api/dynamoDbQueries";
 
 export default defineNitroPlugin(() => {
-
 	const storage = useStorage();
 
 	if (process.env.NODE_ENV === "production") {
-		const driver = redisDriver({
-			base: "{unstorage}",
-			port: 6379,
-			host: useRuntimeConfig().redisEndpoint,
-			password: useRuntimeConfig().redisPassword,
-			username: useRuntimeConfig().redisUsername,
-			tls: {},
+		console.log("building custom driver");
+		const driver = defineDriver(() => {
+			return {
+				name: "dynamoDb-driver",
+				options: {},
+				async hasItem(_key, _opts) {
+					return false;
+				},
+				async getItem(key, _opts) {
+					return getSession(key);
+				},
+				async setItem(key, value, _opts) {
+					return setSession(key, value);
+				},
+				// async removeItem(key, _opts) {},
+				async getKeys(_base, _opts) {
+					return [];
+				},
+			// async clear(base, _opts) {},
+			// async dispose() {},
+			// async watch(callback) {},
+			};
 		});
+
 		storage.mount("cache", driver);
 	}
 });
