4995d7980ad6f7e287f4994ed4479199c5dcae71
Harriet Hall <harriet.hall@madetech.com>
2025-10-24 17:10:49 +0100

    EC-999:  remove comments

--- server/api/session.post.ts
+++ server/api/session.post.ts
@@ -1,34 +1,27 @@
 import { v4 as uuidv4 } from "uuid";
 
 export default defineEventHandler(async (event) => {
-	// Read payload from request body
-	const body = await readBody(event);
+  const body = await readBody(event);
+  let sessionId = getCookie(event, "sessionId");
 
-	// Read session Id from cookie
-	let sessionId = getCookie(event, "sessionId");
+  if (sessionId) {
+    // we may want to do the following to limit the amout of data being sent to the server:
+    // const state = await storage.getItem<EcaasState>(sessionId);
 
-	// If session exists, read state from storage
-	if (sessionId) {
-		// const state = await storage.getItem<EcaasState>(sessionId);
+    // Merge updated partial state with existing state
+    // const newState = merge(state!, body);
 
-		// Merge updated partial state with existing state
-		// const newState = merge(state!, body);
-
-		// Update storage item
-		useStorage("dynamo").setItem(sessionId, body);
-	} else {
-		// Otherwise create new session Id
-		sessionId = uuidv4();
-
-		// Store session Id in cookie
-		setCookie(event, "sessionId", sessionId, {
-			maxAge: 60 * 60 * 24 * 14,
-			httpOnly: true,
-		});
-
+    useStorage("dynamo").setItem(sessionId, body);
+  } else {
+    sessionId = uuidv4();
 		const timestamp = Math.floor(Date.now() / 1000) + 14 * 24 * 60 * 60 * 1000;
-		// Save state to storage
-		useStorage("dynamo").setItem(sessionId, body, { ttl: timestamp });
-	}
-	event.node.res.end();
+
+    setCookie(event, "sessionId", sessionId, {
+      maxAge: 60 * 60 * 24 * 14,
+      httpOnly: true,
+    });
+
+    useStorage("dynamo").setItem(sessionId, body, { ttl: timestamp });
+  }
+  event.node.res.end();
 });
