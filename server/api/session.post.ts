6ae22d3f30590d686b4ce8a19c52349b9195a2ae
Harriet Hall <harriet.hall@madetech.com>
2025-10-28 12:48:45 +0000

    EC-999: refactor api route (session.post)

--- server/api/session.post.ts
+++ server/api/session.post.ts
@@ -1,37 +1,36 @@
+import type { H3Event } from "h3";
 import { v4 as uuidv4 } from "uuid";
 import type { EcaasState } from "~/stores/ecaasStore.schema";
 
+const twoWeeksInSeconds = 14 * 24 * 60 * 60;
+
+async function getSessionId(event: H3Event) {
+	let sessionId = getCookie(event, "sessionId");
+	if (!sessionId) {
+		sessionId = uuidv4();
+
+		setCookie(event, "sessionId", sessionId, {
+			maxAge: twoWeeksInSeconds,
+			httpOnly: true,
+		});
+	}
+	return sessionId;
+}
 export default defineEventHandler(async (event) => {
-	const body = await readBody(event);
-	const twoWeeksInSeconds = 14 * 24 * 60 * 60;
 	const timeToLive = Math.floor(Date.now() / 1000) + twoWeeksInSeconds;
-  let sessionId = getCookie(event, "sessionId");
-	
-  if (sessionId) {
-    const storedSession = await useStorage("dynamo").getItem<EcaasState>(sessionId);
-   
-		if(storedSession){
-			await useStorage("dynamo").setItem(sessionId, body)
-		} else{
-			await setSessionWithTtl(sessionId, body, timeToLive);
-		}
- 
-    // we may want to do the following to limit the amout of data being sent to the server:
-     // Merge updated partial state with existing state
-    // const newState = merge(state!, body);
-  } else {
-    sessionId = uuidv4();
+	const body = await readBody(event);
+	const sessionId = await getSessionId(event);
+	const storedSession = await useStorage("dynamo").getItem<EcaasState>(sessionId);
 
-    setCookie(event, "sessionId", sessionId, {
-      maxAge: twoWeeksInSeconds,
-      httpOnly: true,
-    });
-
-    await setSessionWithTtl(sessionId, body, timeToLive);
-  }
-  event.node.res.end();
+	if (storedSession) {
+		await useStorage("dynamo").setItem(sessionId, body);
+	} else {
+		await useStorage("dynamo").setItem(sessionId, body, { ttl: timeToLive });
+	}
+	event.node.res.end();
 });
 
-async function setSessionWithTtl(sessionId: string, body: EcaasState, timeToLive: number) {
-	await useStorage("dynamo").setItem(sessionId, body, { ttl: timeToLive });
-}
+
+// we may want to do the following to limit the amout of data being sent to the server:
+// Merge updated partial state with existing state
+// const newState = merge(state!, body)
