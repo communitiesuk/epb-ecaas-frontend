312b6e83fc5e6c2bb4cea7aa3fbc7d8170e5657b
Harriet Hall <harriet.hall@madetech.com>
2025-11-07 15:00:11 +0000

    (e2e-tests): update tests to reflect new dwelling details form values

--- e2e-tests/general-details-journey.spec.ts
+++ e2e-tests/general-details-journey.spec.ts
@@ -8,9 +8,18 @@ const fillGeneralDetailsForm = async (page: Page) => {
 
 	//fill out General details form 
 	await page.getByTestId("typeOfDwelling_house").click();
+	await expect(page.getByTestId("typeOfDwelling_house")).toBeChecked();
 	await page.getByTestId("storeysInDwelling").fill("2");
+	await expect(page.getByTestId("storeysInDwelling")).toHaveValue("2");
+	await page.getByTestId("buildingLength").fill("12");
+	await page.getByTestId("buildingWidth").fill("11");
 	await page.getByTestId("numOfBedrooms").fill("3");
-	await page.getByTestId("coolingRequired_yes").click();
+	await page.getByTestId("numOfUtilityRooms").fill("1");
+	await page.getByTestId("numOfBathrooms").fill("3");
+	await page.getByTestId("numOfWCs").fill("2");
+	await page.getByTestId("numOfHabitableRooms").fill("7");
+	await page.getByTestId("numOfRoomsWithTappingPoints").fill("1");
+	await page.getByTestId("fuelType_electricity").click();
 
 	//save form
 	await page.getByTestId("saveAndComplete").click();
@@ -36,25 +45,15 @@ test.describe("General details form data persistence", () => {
 
 		await expect(page.getByTestId("typeOfDwelling_house")).toBeChecked();
 		await expect(page.getByTestId("storeysInDwelling")).toHaveValue("2");
+		await expect(page.getByTestId("buildingLength")).toHaveValue("12");
+		await expect(page.getByTestId("buildingWidth")).toHaveValue("11");
 		await expect(page.getByTestId("numOfBedrooms")).toHaveValue("3");
-		await expect(page.getByTestId("coolingRequired_yes")).toBeChecked();
-	});
-
-	test("should persist data on the Dwelling details summary page", async ({ page }) => {
-
-		await page.click('a[href="/dwelling-details/summary"]');
-
-		const typeOfDwellingElement = page.getByTestId("summary-generalDetails-type-of-dwelling");
-		await expect(typeOfDwellingElement.locator("dd")).toHaveText(/House/);
-
-		const numOfBedroomsElement = page.getByTestId("summary-generalDetails-number-of-storeys-in-building");
-		await expect(numOfBedroomsElement.locator("dd")).toHaveText(/2/);
-
-		const storiesElement = page.getByTestId("summary-generalDetails-number-of-bedrooms");
-		await expect(storiesElement.locator("dd")).toHaveText(/3/);
-
-		const coolingRequiredElement = page.getByTestId("summary-generalDetails-cooling-required");
-		await expect(coolingRequiredElement.locator("dd")).toHaveText(/Yes/);
+		await expect(page.getByTestId("numOfUtilityRooms")).toHaveValue("1");
+		await expect(page.getByTestId("numOfBathrooms")).toHaveValue("3");
+		await expect(page.getByTestId("numOfWCs")).toHaveValue("2");
+		await expect(page.getByTestId("numOfHabitableRooms")).toHaveValue("7");
+		await expect(page.getByTestId("numOfRoomsWithTappingPoints")).toHaveValue("1");
+		await expect(page.getByTestId("fuelType_electricity")).toBeChecked();
 	});
 
 	test("should persist data when page is reloaded", async ({ page }) => {
@@ -64,8 +63,15 @@ test.describe("General details form data persistence", () => {
 
 		await expect(page.getByTestId("typeOfDwelling_house")).toBeChecked();
 		await expect(page.getByTestId("storeysInDwelling")).toHaveValue("2");
+		await expect(page.getByTestId("buildingLength")).toHaveValue("12");
+		await expect(page.getByTestId("buildingWidth")).toHaveValue("11");
 		await expect(page.getByTestId("numOfBedrooms")).toHaveValue("3");
-		await expect(page.getByTestId("coolingRequired_yes")).toBeChecked();
+		await expect(page.getByTestId("numOfUtilityRooms")).toHaveValue("1");
+		await expect(page.getByTestId("numOfBathrooms")).toHaveValue("3");
+		await expect(page.getByTestId("numOfWCs")).toHaveValue("2");
+		await expect(page.getByTestId("numOfHabitableRooms")).toHaveValue("7");
+		await expect(page.getByTestId("numOfRoomsWithTappingPoints")).toHaveValue("1");
+		await expect(page.getByTestId("fuelType_electricity")).toBeChecked();
 	});
 
 	test.skip("should persist data when local storage is cleared and page is reloaded", async ({ page }) => {
@@ -79,7 +85,14 @@ test.describe("General details form data persistence", () => {
 
 		await expect(page.getByTestId("typeOfDwelling_house")).toBeChecked();
 		await expect(page.getByTestId("storeysInDwelling")).toHaveValue("2");
+		await expect(page.getByTestId("buildingLength")).toHaveValue("12");
+		await expect(page.getByTestId("buildingWidth")).toHaveValue("11");
 		await expect(page.getByTestId("numOfBedrooms")).toHaveValue("3");
-		await expect(page.getByTestId("coolingRequired_yes")).toBeChecked();
+		await expect(page.getByTestId("numOfUtilityRooms")).toHaveValue("1");
+		await expect(page.getByTestId("numOfBathrooms")).toHaveValue("3");
+		await expect(page.getByTestId("numOfWCs")).toHaveValue("2");
+		await expect(page.getByTestId("numOfHabitableRooms")).toHaveValue("7");
+		await expect(page.getByTestId("numOfRoomsWithTappingPoints")).toHaveValue("1");
+		await expect(page.getByTestId("fuelType_electricity")).toBeChecked();
 	});
 });
