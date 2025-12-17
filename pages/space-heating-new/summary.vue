92a52d8bce85eb8d5d8eb4a521b360c952828e53
Adil Rahman <232363244+adil-rahman-mt@users.noreply.github.com>
2025-12-17 11:35:57 +0000

    EC-1061 Add simple summary page

---
+++ pages/space-heating-new/summary.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import type { SummarySection } from "~/common.types";
+import { getTabItems, getUrl } from "#imports";
+const store = useEcaasStore();
+const title = "Space heating NEW summary";
+
+const spaceHeatingUrl = "/space-heating-new";
+const heatSources = store.spaceHeatingNew.heatSource.data;
+const heatSourceSummary: SummarySection = {
+	id: "heatSource",
+	label: "Heat sources",
+	data:
+		heatSources.map((heatSource) => {
+			return {
+				Name: show(heatSource.data.name),
+				"Type of heat source": displayHeatSourceType(heatSource.data.typeOfHeatSource),
+			};
+		}) || [],
+	editUrl: spaceHeatingUrl,
+};
+
+</script>
+<template>
+	<Head>
+		<Title>{{ title }}</Title>
+	</Head>
+	<h1 class="govuk-heading-l">{{ title }}</h1>
+	<GovTabs v-slot="tabProps" :items="getTabItems([heatSourceSummary])">
+		<SummaryTab :summary="heatSourceSummary" :selected="tabProps.currentTab === 0">
+			<template #empty>
+				<h2 class="govuk-heading-m">No heat sources added</h2>
+				<NuxtLink class="govuk-link" :to="getUrl('heatSourceCreate')"> 
+					Add heat source
+				</NuxtLink>
+			</template>
+		</SummaryTab>
+	</GovTabs>
+	<GovButton href="/">Return to overview</GovButton>
+</template>
