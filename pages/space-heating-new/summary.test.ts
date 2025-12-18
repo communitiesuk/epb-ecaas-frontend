4ec70060e74b92c83901b45905c024f6ce5e47f3
Harriet Hall <harriet.hall@madetech.com>
2025-12-18 11:02:43 +0000

    EC-1061: add space-heating-NEW summary page (WIP)

---
+++ pages/space-heating-new/summary.test.ts
@@ -0,0 +1,225 @@
+import { renderSuspended } from "@nuxt/test-utils/runtime";
+import { screen } from "@testing-library/vue";
+import SpaceHeatingSummary from "./summary.vue";
+// import { kilowatt, kilowattHour, kilowattPeak } from "~/utils/units/power";
+// import { degrees } from "~/utils/units/angle";
+// import { metre } from "~/utils/units/length";
+
+describe("Space heating summary page", () => {
+	it("displays the correct title", async () => {
+		await renderSuspended(SpaceHeatingSummary);
+		expect(screen.getByRole("heading", { name: "Space heating NEW summary" }));
+	});
+
+	describe.skip("Heat sources section", () => {
+
+	
+	const heatSource1: HeatSourceData = {
+		id: "1b73e247-57c5-26b8-1tbd-83tdkc8c3r8a",
+		name: "Heat source 1",
+		typeOfHeatSource: HeatSourceType.boiler,
+		typeOfBoiler: "combiBoiler",
+		productReference: "BOILER_SMALL",
+		locationOfBoiler: AdjacentSpaceType.heatedSpace,
+	};
+
+		it("displays the heat sources tab", async () => {
+			await renderSuspended(SpaceHeatingSummary);
+			expect(screen.getByRole("link", { name: "Heat sources" })).not.toBeNull();
+		});
+
+		it("displays an empty tab state when no data is present", async () => {
+			await renderSuspended(SpaceHeatingSummary);
+
+			expect(screen.getByText("No heat sources added")).not.toBeNull();
+
+			const addHeatSourceLink: HTMLAnchorElement = screen.getByRole("link", {
+				name: "Add heat source",
+			});
+
+			expect(new URL(addHeatSourceLink.href).pathname).toBe(
+				getUrl("heatSourceCreate"),
+			);
+		});
+
+		it("displays the correct data for the heat sources summary", async () => {
+			const store = useEcaasStore();
+		  store.$patch({
+				spaceHeatingNew: {
+					heatSource: {
+						data: [{ data: heatSource1 }],
+					},
+				},
+			});
+
+			await renderSuspended(SpaceHeatingSummary);
+
+			const expectedResult = {
+				Name: "Heat source 1",
+				"Type of heat source": HeatSourceType.boiler,
+        "Type of boiler" : "Combi boiler",
+        "Product reference": "BOILER_SMALL",
+        "Location of boiler": AdjacentSpaceType.heatedSpace,
+			};
+
+			for (const [key, value] of Object.entries(expectedResult)) {
+				const lineResult = (await screen.findByTestId(`summary-pvSystems-${hyphenate(key)}`));
+				expect(lineResult.querySelector("dt")?.textContent).toBe(key);
+				expect(lineResult.querySelector("dd")?.textContent).toBe(value);
+			}
+		});
+
+	// describe("Electric battery section", () => {
+	// 	const battery: EcaasForm<ElectricBatteryData> = {
+	// 		data: {
+	// 			name: "Acme Model II",
+	// 			capacity: 10,
+	// 			chargeEfficiency: 0.7,
+	// 			location: "inside",
+	// 			gridChargingPossible: false,
+	// 			maximumChargeRate: 6.2,
+	// 			minimumChargeRate: 4.5,
+	// 			maximumDischargeRate: 2.3,
+	// 		},
+	// 	};
+
+	// 	it("displays the battery tab", async () => {
+	// 		await renderSuspended(PVAndElectricBatteriesSummary);
+	// 		expect(screen.getByRole("link", { name: "Electric batteries" })).not.toBeNull();
+	// 	});
+
+	// 	it("displays an empty tab state when no data is present", async () => {
+	// 		await renderSuspended(PVAndElectricBatteriesSummary);
+
+	// 		expect(screen.getByText("No electric batteries added")).not.toBeNull();
+
+	// 		const addPVSystemsLink: HTMLAnchorElement = screen.getByRole("link", {
+	// 			name: "Add electric battery",
+	// 		});
+
+	// 		expect(new URL(addPVSystemsLink.href).pathname).toBe(
+	// 			getUrl("pvAndBatteries"),
+	// 		);
+	// 	});
+
+	// 	it("displays the correct data for the electric battery summary", async () => {
+	// 		const store = useEcaasStore();
+	// 		store.$patch({
+	// 			pvAndBatteries: {
+	// 				electricBattery: {
+	// 					data: [battery],
+	// 				},
+	// 			},
+	// 		});
+
+	// 		await renderSuspended(PVAndElectricBatteriesSummary);
+
+	// 		const expectedResult = {
+	// 			Name: "Acme Model II",
+	// 			Capacity: `10 ${kilowattHour.suffix}`,
+	// 			"Charge efficiency": "0.7",
+	// 			Location: "Inside",
+	// 			"Grid charging possible": "No",
+	// 			"Maximum charge rate": `6.2 ${kilowatt.suffix}`,
+	// 			"Minimum charge rate": `4.5 ${kilowatt.suffix}`,
+	// 			"Maximum discharge rate": `2.3 ${kilowatt.suffix}`,
+	// 		};
+
+	// 		for (const [key, value] of Object.entries(expectedResult)) {
+	// 			const lineResult = (await screen.findByTestId(`summary-electricBattery-${hyphenate(key)}`));
+	// 			expect(lineResult.querySelector("dt")?.textContent).toBe(key);
+	// 			expect(lineResult.querySelector("dd")?.textContent).toBe(value);
+	// 		}
+	// 	});
+	// });
+
+	// describe("Diverters section", () => {
+
+	// 	it("displays the diverter tab", async () => {
+	// 		await renderSuspended(PVAndElectricBatteriesSummary);
+	// 		expect(screen.getByRole("link", { name: "Diverters" })).not.toBeNull();
+	// 	});
+
+	// 	it("displays an empty tab state when no data is present", async () => {
+	// 		await renderSuspended(PVAndElectricBatteriesSummary);
+
+	// 		expect(screen.getByText("No diverters added")).not.toBeNull();
+
+	// 		const addPVSystemsLink: HTMLAnchorElement = screen.getByRole("link", {
+	// 			name: "Add diverter",
+	// 		});
+
+	// 		expect(new URL(addPVSystemsLink.href).pathname).toBe(
+	// 			getUrl("pvAndBatteries"),
+	// 		);
+	// 	});
+
+	// 	it("displays the correct data for the diverters summary", async () => {
+	// 		const store = useEcaasStore();
+
+	// 		const hotWaterCylinderName = "HWC1";
+	// 		const hotWaterCylinderId = "88ea3f45-6f2a-40e2-9117-0541bd8a97f3";
+	// 		const heatPumpName = "HP1";
+	// 		const heatPumpId = "56ddc6ce-7a91-4263-b051-96c7216bb01e";
+
+	// 		const diverter: EcaasForm<PvDiverterData> = {
+	// 			data: {
+	// 				name: "Diverter 1",
+	// 				hotWaterCylinder: hotWaterCylinderId,
+	// 			},
+	// 		};
+
+	// 		store.$patch({
+	// 			spaceHeating: {
+	// 				heatGeneration: {
+	// 					heatPump: {
+	// 						data: [{
+	// 							data: {
+	// 								name: heatPumpName,
+	// 								id: heatPumpId,
+	// 								productReference: "HEATPUMP-SMALL",
+	// 							},
+	// 						}],
+	// 					},
+	// 				},
+	// 			},
+	// 			domesticHotWater: {
+	// 				waterHeating: {
+	// 					hotWaterCylinder: {
+	// 						data: [{
+	// 							data: {
+	// 								name: hotWaterCylinderName,
+	// 								id: hotWaterCylinderId,
+	// 								heatSource: heatPumpId,
+	// 								storageCylinderVolume: {
+	// 									amount: 1,
+	// 									unit: "litres",
+	// 								},
+	// 								dailyEnergyLoss: 1,
+	// 							},
+	// 						}],
+	// 					},
+	// 				},
+	// 			},
+	// 			pvAndBatteries: {
+	// 				diverters: {
+	// 					data: [diverter],
+	// 				},
+	// 			},
+	// 		});
+
+	// 		await renderSuspended(PVAndElectricBatteriesSummary);
+
+	// 		const expectedResult = {
+	// 			"Name": "Diverter 1",
+	// 			"Associated hot water cylinder": hotWaterCylinderName,
+	// 		};
+
+	// 		for (const [key, value] of Object.entries(expectedResult)) {
+	// 			const lineResult = await screen.findByTestId(`summary-diverters-${hyphenate(key)}`);
+	// 			expect(lineResult.querySelector("dt")?.textContent).toBe(key);
+	// 			expect(lineResult.querySelector("dd")?.textContent).toBe(value);
+	// 		}
+	// 	});
+	});
+});
--- pages/space-heating-new/summary.vue
+++ pages/space-heating-new/summary.vue
@@ -1,28 +1,175 @@
 <script setup lang="ts">
 import type { SummarySection } from "~/common.types";
-import { getTabItems, getUrl } from "#imports";
+import { getTabItems, getUrl, HeatSourceType } from "#imports";
+
 const store = useEcaasStore();
 const title = "Space heating NEW summary";
 
 const spaceHeatingUrl = "/space-heating-new";
 
 const heatSources = store.spaceHeatingNew.heatSource.data;
-const heatSourceSummary: SummarySection = {
-	id: "heatSource",
+const boilers = heatSources.filter(x => x.data.typeOfHeatSource === HeatSourceType.boiler)
+const heatPumps = heatSources.filter(x => x.data.typeOfHeatSource === HeatSourceType.heatPump)
+// const heatNetworks = heatSources.filter(x => x.data.typeOfHeatSource === HeatSourceType.heatNetwork)
+const heatNetworks: [] = []
+const heatBatteries = heatSources.filter(x => x.data.typeOfHeatSource === HeatSourceType.heatBattery)
+const solarThermalSystem = heatSources.filter(x => x.data.typeOfHeatSource === HeatSourceType.solarThermalSystem)
+
+const heatSourcesSummary: SummarySection = {
+	id: "heatSourceSummary",
 	label: "Heat sources",
+	data:[],
+	editUrl: spaceHeatingUrl
+}
+
+const boilerSummary: SummarySection = {
+	id: "boilerSummary",
+	label: "Boilers",
 	data:
-		heatSources.map((heatSource) => {
-			return {
-				Name: show(heatSource.data.name),
-				"Type of heat source": displayHeatSourceType(heatSource.data.typeOfHeatSource),
-			};
+		boilers.map(({data: heatSource}) => {
+
+			const summary = {
+				Name: show(heatSource.name),
+				"Type of heat source": displayHeatSourceType(heatSource.typeOfHeatSource),
+				"Type of boiler": "typeOfBoiler" in heatSource ? displayCamelToSentenceCase(heatSource.typeOfBoiler): emptyValueRendering,
+				"Product reference": "productReference" in heatSource ? heatSource.productReference : emptyValueRendering,
+				"Location of boiler": "locationOfBoiler" in heatSource ? displayCamelToSentenceCase(heatSource.locationOfBoiler): emptyValueRendering, 
+			}
+			return summary
 		}) || [],
 	editUrl: spaceHeatingUrl,
 };
 
+const heatPumpSummary: SummarySection = {
+	id: "heatPumpSummary",
+	label: "Heat pumps",
+	data:
+		heatPumps.map(({data: heatSource}) => {
+
+			const summary = {
+				Name: show(heatSource.name),
+				"Type of heat source": displayHeatSourceType(heatSource.typeOfHeatSource),
+				"Type of heat pump":  "typeOfHeatPump" in heatSource ? displayCamelToSentenceCase(heatSource.typeOfHeatPump) : emptyValueRendering,
+				"Product reference": "productReference" in heatSource ? heatSource.productReference: emptyValueRendering,
+			}
+			return summary
+		}) || [],
+	editUrl: spaceHeatingUrl,
+};
+
+const heatNetworkSummary: SummarySection = {
+	id: "heatNetworkSummary",
+	label: "Heat networks",
+	data:
+		heatNetworks.map(({data: heatSource}) => {
+
+			const summary = {
+				// Name: show(heatSource.name),
+				// "Type of heat source": displayHeatSourceType(heatSource.typeOfHeatSource),
+				// "Type of heat network": "typeOfHeatNetwork" in heatSource && displayCamelToSentenceCase(heatSource.typeOfHeatNetwork),
+				// "Product reference": "productReference" in heatSource && heatSource.productReference,
+			}
+			return summary
+		}) || [],
+	editUrl: spaceHeatingUrl,
+};
+
+const heatBatterySummary: SummarySection = {
+	id: "heatBatterySummary",
+	label: "Heat batteries",
+	data:
+		heatBatteries.map(({data: heatSource}) => {
+
+			const summary = {
+				Name: show(heatSource.name),
+				"Type of heat source": displayHeatSourceType(heatSource.typeOfHeatSource),
+				"Type of heat battery": "typeOfHeatBattery" in heatSource ? displayCamelToSentenceCase(heatSource.typeOfHeatBattery): emptyValueRendering,
+				"Product reference": "productReference" in heatSource ? heatSource.productReference: emptyValueRendering,
+				"Number of units": "numberOfUnits" in heatSource ? heatSource.numberOfUnits: emptyValueRendering,
+				// "Energy supply": "energySupply" in heatSource ? displayFuelType(heatSource.energySupply): emptyValueRendering, //TODO
+			}
+			return summary
+		}) || [],
+	editUrl: spaceHeatingUrl,
+};
+
+const solarThermalSystemSummary: SummarySection = {
+	id: "solarThermalSystemSummary",
+	label: "Solar thermal system",
+	data:
+		solarThermalSystem.map(({data: heatSource}) => {
+
+			const summary = {
+			Name: show(heatSource.name),
+			"Type of heat source": displayHeatSourceType(heatSource.typeOfHeatSource),
+
+			"Location of collector loop piping":
+				"locationOfCollectorLoopPiping" in heatSource
+					? displayCamelToSentenceCase(heatSource.locationOfCollectorLoopPiping)
+					: emptyValueRendering,
+
+			"Collector module area": 
+				"collectorModuleArea" in heatSource ? heatSource.collectorModuleArea : emptyValueRendering,
+
+			"Number of collector modules": 
+				"numberOfCollectorModules" in heatSource ? heatSource.numberOfCollectorModules : emptyValueRendering,
+
+			"Peak collector efficiency": 
+				"peakCollectorEfficiency" in heatSource ? heatSource.peakCollectorEfficiency : emptyValueRendering,
+
+			"Incidence angle modifier": 
+				"incidenceAngleModifier" in heatSource ? heatSource.incidenceAngleModifier : emptyValueRendering,
+
+			"First order heat loss coefficient": 
+				"firstOrderHeatLossCoefficient" in heatSource ? heatSource.firstOrderHeatLossCoefficient : emptyValueRendering,
+
+			"Second order heat loss coefficient": 
+				"secondOrderHeatLossCoefficient" in heatSource ? heatSource.secondOrderHeatLossCoefficient : emptyValueRendering,
+
+			"Heat loss coefficient of solar loop piping": 
+				"heatLossCoefficientOfSolarLoopPipe" in heatSource
+					? heatSource.heatLossCoefficientOfSolarLoopPipe
+					: emptyValueRendering,
+
+			"Collector mass flow rate": 
+				"collectorMassFlowRate" in heatSource ? heatSource.collectorMassFlowRate : emptyValueRendering,
+
+			"Power of collector pump": 
+				"powerOfCollectorPump" in heatSource ? heatSource.powerOfCollectorPump : emptyValueRendering,
+
+			"Power of collector pump controller": 
+				"powerOfCollectorPumpController" in heatSource
+					? heatSource.powerOfCollectorPumpController
+					: emptyValueRendering,
+
+			"Pitch": 
+				"pitch" in heatSource ? dim(heatSource.pitch, "degrees"): emptyValueRendering,
+
+			"Orientation": 
+				"orientation" in heatSource ? dim(heatSource.orientation, "degrees"): emptyValueRendering,
+}
+
+			return summary
+		}) || [],
+	editUrl: spaceHeatingUrl,
+};
+
+function getNonEmptySections(summarySections: SummarySection[]){
+	return summarySections.filter(x => Array.isArray(x.data) && x.data.length > 0)
+}
+
+const heatSourceSections: SummarySection[] = [
+	heatSourcesSummary,
+	boilerSummary,
+	heatPumpSummary,
+	heatNetworkSummary,
+	heatBatterySummary,
+	solarThermalSystemSummary
+];
+
 const heatingControls = store.spaceHeatingNew.heatingControls.data;
 const heatingControlsSummary: SummarySection = {
-	id: "heatingControla",
+	id: "heatingControls",
 	label: "Heating controls",
 	data:
 		heatingControls.map( ({ data: x } ) => {
@@ -39,15 +186,17 @@ const heatingControlsSummary: SummarySection = {
 		<Title>{{ title }}</Title>
 	</Head>
 	<h1 class="govuk-heading-l">{{ title }}</h1>
-	<GovTabs v-slot="tabProps" :items="getTabItems([heatSourceSummary])">
-		<SummaryTab :summary="heatSourceSummary" :selected="tabProps.currentTab === 0">
-			<template #empty>
-				<h2 class="govuk-heading-m">No heat sources added</h2>
-				<NuxtLink class="govuk-link" :to="getUrl('heatSourceCreate')"> 
-					Add heat source
-				</NuxtLink>
-			</template>
-		</SummaryTab>
+	<GovTabs v-slot="tabProps" :items="getNonEmptySections(heatSourceSections)">
+		<template v-for="section, i of getNonEmptySections(heatSourceSections)">
+			<SummaryTab :summary="section" :selected="tabProps.currentTab === i">
+				<template #empty>
+					<h2 class="govuk-heading-m">No heat sources added</h2>
+					<NuxtLink class="govuk-link" :to="getUrl('heatSourceCreate')"> 
+						Add heat source
+					</NuxtLink>
+				</template>
+			</SummaryTab>
+		</template>
 	</GovTabs>
 	<GovTabs v-slot="tabProps" :items="getTabItems([heatingControlsSummary])">
 		<SummaryTab :summary="heatingControlsSummary" :selected="tabProps.currentTab === 0">
