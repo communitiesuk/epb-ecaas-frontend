e13e75109ed0a10e2a84fef0a47021fada5a59e3
James Wainwright <james.wainwright@madetech.com>
2026-01-07 17:26:09 +0000

    Adapt breadcrumbs to handle dynamic route params

--- components/gov/Breadcrumbs.vue
+++ components/gov/Breadcrumbs.vue
@@ -2,6 +2,8 @@
 import type { Page } from "~/data/pages/pages.types";
 import pagesData from "../../data/pages/pages";
 
+const route = useRoute();
+
 const getBreadcrumbs = (id: string | undefined, breadcrumbs: Array<Page>): Array<Page> => {
 	const currentPage = pagesData.find(page => page.id === id);
 
@@ -13,8 +15,21 @@ const getBreadcrumbs = (id: string | undefined, breadcrumbs: Array<Page>): Array
 	return getBreadcrumbs(currentPage.parentId, breadcrumbs);
 };
 
+const getBreadcrumbUrl = (page: Page) => {
+	let path = page.url;
+
+	const routeSegments = route.path.split("/");
+	const paramSegments = page.url.split("/").filter(p => p.startsWith(":"));
+
+	if (paramSegments.length) {
+		routeSegments.splice(routeSegments.length - paramSegments.length, paramSegments.length);
+		path = routeSegments.join("/");
+	}
+
+	return path;
+};
+
 const pages = computed(() => {
-	const route = useRoute();
 	const params = Object.keys(route.params);
 
 	let path = route.path;
@@ -22,7 +37,7 @@ const pages = computed(() => {
 
 	if (!currentPage && params.length) {
 		const segments = route.path.split("/");
-		segments.splice(segments.length - params.length, 1);
+		segments.splice(segments.length - params.length, params.length);
 
 		params.forEach(p => segments.push(`:${p}`));
 
@@ -39,7 +54,7 @@ const pages = computed(() => {
 	<nav v-if="pages.length > 1" class="govuk-breadcrumbs govuk-!-margin-top-0 govuk-!-margin-bottom-6" aria-label="Breadcrumb">
 		<ol class="govuk-breadcrumbs__list">
 			<li v-for="(page, index) in pages" :key="page.id" class="govuk-breadcrumbs__list-item">
-				<NuxtLink v-if="index !== pages.length - 1" class="govuk-breadcrumbs__link" :to="page.url">
+				<NuxtLink v-if="index !== pages.length - 1" class="govuk-breadcrumbs__link" :to="getBreadcrumbUrl(page)">
 					{{ page.title }}
 				</NuxtLink>
 				<span v-else>{{ page.title }}</span>
