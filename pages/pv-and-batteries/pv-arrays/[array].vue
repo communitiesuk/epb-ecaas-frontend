327ed6520d433ae1475fc2c31f601fd3f0625ad4
Mark Tyree <tyreemarka@gmail.com>
2026-02-23 16:08:00 +0000

    EC-1241 (PV Shading) -- changes heading font size and removed guidance link temporarily

--- pages/pv-and-batteries/pv-arrays/[array].vue
+++ pages/pv-and-batteries/pv-arrays/[array].vue
@@ -317,10 +317,10 @@ const { handleInvalidSubmit, errorMessages } = useErrorSummary();
 			validation="required"
 		/>
 		<hr class="govuk-section-break govuk-section-break--l govuk-section-break--visible">
-		<h2 class="govuk-heading-m">PV array shading</h2>
-		<p class="govuk-body">
+		<h2 class="govuk-heading-l">PV array shading</h2>
+		<!-- <p class="govuk-body">
 			<a href="/guidance/pv-shading" target="_blank" class="govuk-link">Guidance on shading (opens in another window)</a>
-		</p>
+		</p> -->
 		<FormKit
 			id="hasShading"
 			type="govBoolean"
