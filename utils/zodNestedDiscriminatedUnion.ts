db53a61d0aa9c5d094c04b8902b462d80d55cebd
Jasper Bailey <jasperbailey98@gmail.com>
2026-03-02 09:31:07 +0000

    EC-1268: remove now unnecessary n=1 and n=2 cases

--- utils/zodNestedDiscriminatedUnion.ts
+++ utils/zodNestedDiscriminatedUnion.ts
@@ -2,8 +2,6 @@ import * as z from "zod";
 
 type DiscUnionParams = { discriminator: string, variants: [z.ZodObject, ...z.ZodObject[]] };
 
-type DiscriminableTuple = [z.core.$ZodTypeDiscriminable, ...z.core.$ZodTypeDiscriminable[]];
-
 function mapTuple<T extends object[], U>(
 	tuple: T,
 	fn: (item: T[number], index: number,) => U,
@@ -68,89 +66,3 @@ export type NDUNFromUnions<
   				Unions[0]["discriminator"]
   			>
   		: never;
-
-export function ndu1<C extends z.ZodObject, U extends DiscUnionParams>(
-	commonFields: C,
-	union: U,
-): NDU1FromUnions<C, U> {
-	const variants = mapTuple(
-		union.variants,
-		v => commonFields.extend(v.shape),
-	);
-
-	return z.discriminatedUnion(
-		union.discriminator,
-		variants,
-	) as NDU1FromUnions<C, U>;
-}
-
-export type NDU1<
-	Common extends z.ZodObject,
-	Types extends DiscriminableTuple,
-	Disc extends string,
->
-	= z.ZodDiscriminatedUnion<
-		{
-			[K in keyof Types]: Types[K] & Common
-		},
-		Disc
-	>;
-
-export type NDU1FromUnions<
-	Common extends z.ZodObject,
-	Union extends DiscUnionParams,
->
-	= z.ZodDiscriminatedUnion<
-		{
-			[K in keyof Union["variants"]]: K extends number
-				? Union["variants"][K] & Common
-				: Union["variants"][K]
-		},
-		Union["discriminator"]
-	>;
-
-export function ndu2<C extends z.ZodObject, U extends [DiscUnionParams, DiscUnionParams]>(
-	commonFields: C,
-	...unions: U
-): NDU2FromUnions<C, U> {
-	const variants = mapTuple(
-		unions[0].variants,
-		v => ndu1(commonFields.extend(v.shape), unions[1]),
-	);
-
-	return z.discriminatedUnion(
-		unions[0].discriminator,
-		variants,
-	) as NDU2FromUnions<C, U>;
-}
-
-export type NDU2<
-	Common extends z.ZodObject,
-	Types extends readonly [
-		DiscriminableTuple,
-		DiscriminableTuple,
-	],
-	Disc extends [string, string],
-> = z.ZodDiscriminatedUnion<
-	{
-		[K in keyof Types[0]]: K extends number ? NDU1<
-      Types[0][K] & Common,
-			Types[1],
-			Disc[1]
-		> : Types[0][K]
-	},
-	Disc[0]
->;
-
-export type NDU2FromUnions<
-	Common extends z.ZodObject,
-	Unions extends [DiscUnionParams, DiscUnionParams],
-> = z.ZodDiscriminatedUnion<
-	{
-		[K in keyof Unions[0]["variants"]]: K extends number ? NDU1FromUnions<
-      Unions[0]["variants"][K] & Common,
-			Unions[1]
-		> : Unions[0]["variants"][K]
-	},
-	Unions[0]["discriminator"]
->;
