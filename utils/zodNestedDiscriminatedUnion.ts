ac9e2d16b947a6aa10875dc3a110d59ea24eafb1
Jasper Bailey <jasperbailey98@gmail.com>
2026-03-02 10:45:39 +0000

    EC-1268: Add docstring and explanatory comments

--- utils/zodNestedDiscriminatedUnion.ts
+++ utils/zodNestedDiscriminatedUnion.ts
@@ -1,17 +1,26 @@
 import * as z from "zod";
 
-type DiscUnionParams = {
+export type DiscUnionParams = {
 	discriminator: string,
 	variants: [z.ZodObject, ...z.ZodObject[]]
 };
+
 
 function mapTuple<T extends object[], U>(
 	tuple: T,
 	fn: (item: T[number], index: number,) => U,
 ): { [K in keyof T]: U } {
+	// .map() and similar don't preserve tuple types in TypeScript, which is
+	// annoying as this typecast loses some specificity
 	return tuple.map(fn) as { [K in keyof T]: U };
 }
 
+/** 
+ * @param commonFields A ZodObject with any fields which are identical throughout the union. Discriminator properties should not be included here.
+ * @param unions A non-empty array containing `DiscUnionParams` objects
+ * @returns A Zod schema which will validate a flat object structure with any
+ * permutation of the discriminated unions provided.
+ */
 export function nestedDiscriminatedUnion<
 	C extends z.ZodObject, 
 	U extends [DiscUnionParams, ...DiscUnionParams[]],
@@ -20,7 +29,8 @@ export function nestedDiscriminatedUnion<
 	...unions: U
 ): NDUNFromUnions<C, U> {
 	if (unions.length === 0) {
-		throw new RangeError("can't be giving us an empty array mate");
+		// If this is reached, there should already be a ts-error where it's called
+		throw new RangeError("`unions` parameter is empty - expected at least one element");
 	}
 
 	if (unions.length === 1) {
@@ -33,6 +43,7 @@ export function nestedDiscriminatedUnion<
 			unions[0].discriminator,
 			variants,
 		) as unknown as NDUNFromUnions<C, U>;
+		// These type casts are only safe because we're testing NDUNFromUnions quite extensively - potentially brittle
 	} 
 
 	const variants = mapTuple(
@@ -44,17 +55,22 @@ export function nestedDiscriminatedUnion<
 		unions[0].discriminator,
 		variants,
 	) as unknown as NDUNFromUnions<C, U>;
+	// These type casts are only safe because we're testing NDUNFromUnions quite extensively - potentially brittle
 }
 
 export type NDUNFromUnions<
 	Common extends z.ZodObject,
 	Unions extends readonly [DiscUnionParams, ...DiscUnionParams[]],
 > =
+	// always true, just used to isolate Rest to check if it's empty later
   	Unions extends [infer _First, ...infer Rest]
   		? z.ZodDiscriminatedUnion<
   			{ 
   				[K in keyof Unions[0]["variants"]]:
+  				// We only want to extend the properties of the array given by index
+  				// keys, stuff like .concat() and .length should just be copied
   				K extends number
+  					// If Rest is empty, we don't need to recurse any further
   					? Rest extends [DiscUnionParams, ...DiscUnionParams[]]
   						? NDUNFromUnions<Common & Unions[0]["variants"][K], Rest>
   						: Unions[0]["variants"][K] & Common
