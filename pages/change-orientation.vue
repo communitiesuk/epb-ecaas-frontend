8f108e9a5976ec45ad33c29ebbfa9224fb350a50
Harriet Hall <harriet.hall@madetech.com>
2026-03-03 11:05:40 +0000

    EC-1099 (change orientation page): make orientation a computed property so its updated automatically on form

--- pages/change-orientation.vue
+++ pages/change-orientation.vue
@@ -12,16 +12,22 @@ const saveForm = () => {
 };
 const { dwellingSpaceExternalGlazedDoor, dwellingSpaceExternalUnglazedDoor, dwellingSpaceInternalDoor } = store.dwellingFabric.dwellingSpaceDoors;
 const doors = [ dwellingSpaceExternalGlazedDoor.data, dwellingSpaceExternalUnglazedDoor.data, dwellingSpaceInternalDoor.data ].flat();
-
 const frontDoor = doors.find(door => door.data.isTheFrontDoor);
 const { dwellingSpaceExternalWall } = store.dwellingFabric.dwellingSpaceWalls;
 const { dwellingSpaceRoofs } = store.dwellingFabric.dwellingSpaceCeilingsAndRoofs;
-const taggedItem = store.getTaggedItem([dwellingSpaceExternalWall, dwellingSpaceRoofs], frontDoor?.data.associatedItemId);
+const pvs = store.pvAndBatteries.pvArrays.data;
+const externalWalls = store.dwellingFabric.dwellingSpaceWalls.dwellingSpaceExternalWall.data;
+const roofs = store.dwellingFabric.dwellingSpaceCeilingsAndRoofs.dwellingSpaceRoofs.data;
+const windows = store.dwellingFabric.dwellingSpaceWindows.data;
 
-let orientation; 	
-if (frontDoor) {
-	orientation = "orientation" in frontDoor.data ? frontDoor.data.orientation : taggedItem?.orientation;
-}
+const itemTaggedToDoor = store.getTaggedItem([dwellingSpaceExternalWall, dwellingSpaceRoofs], frontDoor?.data.associatedItemId);
+
+const orientationOfFrontDoor = computed(() => {
+	if (!frontDoor) return;
+	return "orientation" in frontDoor.data ? frontDoor.data.orientation : itemTaggedToDoor?.orientation;
+
+});
+
 type ChangeOrientationModel = {
 	name?: string,
 	orientation?: number, 
@@ -29,25 +35,18 @@ type ChangeOrientationModel = {
 };
 const model = ref<ChangeOrientationModel>({
 	name: frontDoor?.data.name,
-	orientation: orientation ?? taggedItem?.orientation,
 	newOrientation: undefined,
 });
 
-function getDiffInOrientation(model: globalThis.Ref<ChangeOrientationModel>) {
-
-	if (model.value.orientation === undefined || model.value.newOrientation === undefined) return;
-	return model.value.newOrientation - model.value.orientation; 
+function getDiffInOrientation() {
+	if (orientationOfFrontDoor.value === undefined || model.value.newOrientation === undefined) return;
+	return model.value.newOrientation - orientationOfFrontDoor.value; 
 }
 
-const pvs = store.pvAndBatteries.pvArrays.data;
-const externalWalls = store.dwellingFabric.dwellingSpaceWalls.dwellingSpaceExternalWall.data;
-const roofs = store.dwellingFabric.dwellingSpaceCeilingsAndRoofs.dwellingSpaceRoofs.data;
-const windows = store.dwellingFabric.dwellingSpaceWindows.data;
 const items = [...doors, pvs, externalWalls, roofs, windows].flat();
 
 const changeOrientationOfItems = () => {
-
-	const difference = getDiffInOrientation(model);
+	const difference = getDiffInOrientation();
 	if (difference) {
 		for (const item of items) {
 			if ("orientation" in item.data && item.data.orientation !== undefined) {
@@ -84,7 +83,7 @@ const changeOrientationOfItems = () => {
 			</div>
 			<ul class="govuk-list">
 				<li>Door: <span data-testid="frontDoorName" class="bold">{{ model?.name }}</span></li>
-				<li>Orientation: <span data-testid="currentOrientation" class="bold">{{ model.orientation}}</span></li>
+				<li>Orientation: <span data-testid="currentOrientation" class="bold">{{ orientationOfFrontDoor }}</span></li>
 			</ul>
 		</div>
 		<FormKit
@@ -93,10 +92,10 @@ const changeOrientationOfItems = () => {
 			type="govInputWithSuffix"
 			label="New orientation of the front door"
 			help="Enter the new orientation of the front door measured clockwise from true north. All other elements will be moved in accordance with this door."
-			validation="number | min:1 | max:359" 
+			validation="number | min:0 | max:360" 
 			suffix-text="°"
 		/>
-		<GovButton :href="'/'" :click="changeOrientationOfItems" test-id="changeOrientationButton">Change orientation</GovButton>
+		<GovButton test-id="changeOrientationButton" @click="changeOrientationOfItems" >Change orientation</GovButton>
 	</formkit>
 </template>
 
