24a4dc20a344ee6d56662f65771a16d459a76031
Mark Tyree <tyreemarka@gmail.com>
2026-03-25 09:22:25 +0000

    EC-1320 removes unecessary information for secondary pipework and changes image

--- components/links/PipeworkGuidance.vue
+++ components/links/PipeworkGuidance.vue
@@ -2,7 +2,7 @@
 	<div class="govuk-inset-text">
 		<p>		You only need to add information about the primary pipework connecting a heat source to a thermal store, for example a hot water cylinder. Secondary and tertiary pipework is estimated in the calculation.
 		</p>
-		<a href="/guidance/primary-and-secondary-pipework" target="_blank" class="govuk-link">
+		<a href="/guidance/primary-pipework" target="_blank" class="govuk-link">
 			Guidance on pipework (opens in another window)
 		</a>
 	</div>
