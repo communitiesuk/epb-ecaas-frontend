3e5f60c3d0a983e0fa5b764865fb3260698228bd
Mark Tyree <tyreemarka@gmail.com>
2026-03-25 09:47:42 +0000

    EC-1299 (Space heating controls) adds help with input table

--- pages/space-heating/heating-controls/index.vue
+++ pages/space-heating/heating-controls/index.vue
@@ -73,8 +73,29 @@ const { handleInvalidSubmit, errorMessages } = useErrorSummary();
 			label="Type of heating control for dwelling"
 			name="heatingControlType"
 			validation="required"
-		/>
-		<GovLLMWarning />
+		><GovDetails
+			class="summary-text"
+			:summary-text="`Help with this input`"
+			classes="govuk-!-margin-bottom-4">
+			<table class="govuk-table">
+				<thead class="govuk-table__head">
+					<tr class="govuk-table__row">
+						<th class="govuk-table__header">Type of control</th>
+						<th class="govuk-table__header">Description</th>
+					</tr>
+				</thead>
+				<tbody class="govuk-table__body">
+					<tr class="govuk-table__row">
+						<td class="govuk-table__cell">Separate temperature</td>
+						<td class="govuk-table__cell">This is, for example, where there is a room thermostat in one room and thermostatic radiator valves, but all rooms follow a single time schedule.</td>
+					</tr>
+					<tr class="govuk-table__row">
+						<td class="govuk-table__cell">Separate time and temperature</td>
+						<td class="govuk-table__cell">This is, for example, where there is independent heating schedules and target temperatures for different areas.</td>
+					</tr>
+				</tbody>
+			</table>
+		</GovDetails></FormKit>
 		<div class="govuk-button-group">
 			<FormKit type="govButton" label="Save and mark as complete" test-id="saveAndComplete" :ignore="true" />
 			<GovButton :href="getUrl('spaceHeating')" secondary>Save progress</GovButton>
