e50d2131d3b86a82c31c243ff740cc4553f661c9
Hard-Luck <tyreemarka@gmail.com>
2026-05-13 16:17:47 +0100

    Adds min and max to unit type

--- utils/units/zod.test.ts
+++ utils/units/zod.test.ts
@@ -18,4 +18,47 @@ describe("zodUnit", () => {
 		const zodLength = zodUnit("length");
 		expect(() => zodLength.parse(unitValue(4, litrePerSecond))).toThrowError();
 	});
+	describe("constraints", () => {
+		it("does not consider a number below the minimum as valid", () => {
+			const zodLength = zodUnit("length", { min: 0 });
+			expect(() => zodLength.parse(unitValue(-1, metre))).toThrowError();
+		});
+		it("considers a number above the minimum as valid", () => {
+			const zodLength = zodUnit("length", { min: 0 });
+			expect(zodLength.parse(unitValue(1, metre))).toBeTruthy();
+		});
+		it("does not consider a number above the maximum as valid", () => {
+			const zodLength = zodUnit("length", { max: 10 });
+			expect(() => zodLength.parse(unitValue(11, metre))).toThrowError();
+		});
+		it("considers a number below the maximum as valid", () => {
+			const zodLength = zodUnit("length", { max: 10 });
+			expect(zodLength.parse(unitValue(9, metre))).toBeTruthy();
+		});
+		it("considers a number within the minimum and maximum as valid", () => {
+			const zodLength = zodUnit("length", { min: 0, max: 10 });
+			expect(zodLength.parse(unitValue(5, metre))).toBeTruthy();
+		});
+		it("considers maximum and minimum as inclusive", () => {
+			const zodLength = zodUnit("length", { min: 0, max: 10 });
+			expect(zodLength.parse(unitValue(0, metre))).toBeTruthy();
+			expect(zodLength.parse(unitValue(10, metre))).toBeTruthy();
+		});
+		it("does not consider a min that is greater than the max as valid", () => {
+			expect(() => zodUnit("length", { min: 10, max: 0 })).toThrowError();
+			expect(() => zodUnit("length", { min: -10, max: -20 })).toThrowError();
+		});
+		it("it considers max invalid when set to exclusive", () => {
+			const zodLength = zodUnit("length", {
+				min: 0, max: 10, exclusiveMax: true,
+			});
+			expect(() => zodLength.parse(unitValue(10, metre))).toThrowError();
+		});
+		it("it considers min invalid when set to exclusive", () => {
+			const zodLength = zodUnit("length", {
+				min: 0, max: 10, exclusiveMin: true,
+			});
+			expect(() => zodLength.parse(unitValue(0, metre))).toThrowError();
+		});
+	});
 });
--- utils/units/zod.ts
+++ utils/units/zod.ts
@@ -4,10 +4,40 @@ import { units } from "./units";
 
 type EnumForStringLiteral<T extends string> = { [K in T]: K };
 type ZodObjectForDimension<T extends DimensionName> = z.ZodObject<{ amount: z.ZodNumber, unit: z.ZodEnum<EnumForStringLiteral<UnitForDimension<T>["name"]>> }>;
-
-export function zodUnit<T extends DimensionName>(dimension: T): ZodObjectForDimension<T> {
+type ConstraintsForDimension = {
+	min?: number;
+	max?: number;
+	exclusiveMin?: boolean;
+	exclusiveMax?: boolean;
+};
+export function zodUnit<T extends DimensionName>(dimension: T, constraints: ConstraintsForDimension = {}): ZodObjectForDimension<T> {
+	const amount = createAmountSchema(constraints);
 	return z.object({
-		amount: z.number(),
+		amount,
 		unit: z.enum(units.filter<UnitForDimension<T>>((unit): unit is UnitForDimension<T> => unit.dimension === dimension).map(unit => unit.name)),
 	});
+}
+
+function createAmountSchema(constraints: ConstraintsForDimension): z.ZodNumber {
+	validateConstraints(constraints);
+	let amountSchema = z.number();
+	if (!constraints) return amountSchema;
+	if (constraints.min !== undefined) {
+		amountSchema = constraints.exclusiveMin ? amountSchema.gt(constraints.min) : amountSchema.min(constraints.min);
+	}
+	if (constraints.max !== undefined) {
+		amountSchema = constraints.exclusiveMax ? amountSchema.lt(constraints.max) : amountSchema.max(constraints.max);
+	}
+	return amountSchema;
+}
+
+function validateConstraints(constraints: ConstraintsForDimension): void {
+	if (constraints.min !== undefined && constraints.max !== undefined) {
+		if (constraints.min > constraints.max) {
+			throw new Error("Minimum constraint cannot be greater than maximum constraint");
+		}
+		if (constraints.exclusiveMin && constraints.exclusiveMax && constraints.min === constraints.max) {
+			throw new Error("Minimum and maximum cannot be equal when both are exclusive");
+		}
+	}
 }
