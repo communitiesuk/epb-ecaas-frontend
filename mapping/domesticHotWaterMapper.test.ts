6bcd6d6b55fb8b2cefe97db14220859cea4c2eb6
Douglas Greenshields <douglas.greenshields@communities.gov.uk>
2026-09-17 12:59:38 +0100

    correct creation of CombiBoiler object for API by ensuring product_reference field is present

--- mapping/domesticHotWaterMapper.test.ts
+++ mapping/domesticHotWaterMapper.test.ts
@@ -698,6 +698,7 @@ describe("domestic hot water mapper", () => {
 								type: "CombiBoiler",
 								HeatSourceWet: combiBoiler.data.name,
 								ColdWaterSource: "mains water",
+								product_reference: combiBoiler.data.productReference,
 							},
 						},
 						HeatSourceWet: {
@@ -1289,6 +1290,7 @@ describe("domestic hot water mapper", () => {
 								ColdWaterSource: "mains water",
 								HeatSourceWet: existingCombiBoiler.data.name,
 								type: "CombiBoiler",
+								product_reference: existingCombiBoiler.data.productReference,
 							},
 						},
 						HeatSourceWet: {
