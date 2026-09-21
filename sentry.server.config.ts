9998a49032c7963069ccb16279229d193c46fac1
Douglas Greenshields <douglas.greenshields@communities.gov.uk>
2026-09-21 17:57:55 +0100

    remove dotenv from project as nuxt 3 already implements this functionality

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,11 +1,12 @@
 import * as Sentry from "@sentry/nuxt";
-import dotenv from "dotenv";
 
 // This project is configured to not log to Sentry for development.
 // To temporarily enable Sentry for development, use this command to start the server:
 // NUXT_PUBLIC_ENVIRONMENT='development' npm run dev
-if (process.env.NUXT_PUBLIC_ENVIRONMENT) {
-	dotenv.config();
+// and also define a NUXT_PUBLIC_SENTRY_DSN environment variable
+const { public: { environment, sentryDsn } } = useRuntimeConfig();
+
+if (environment && sentryDsn) {
 	Sentry.init({
 		dsn: process.env.SENTRY_DSN,
 		environment: `cpl-frontend-${process.env.NUXT_PUBLIC_ENVIRONMENT ?? "development"}`,
