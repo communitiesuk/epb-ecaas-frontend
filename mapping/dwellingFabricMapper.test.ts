2ef883d210f84a2ef87551a368162013009efeca
Douglas Greenshields <douglas.greenshields@communities.gov.uk>
2026-09-23 11:37:47 +0100

    temporarily stop transparent building elements sending reveal shading (as broken in FHS 1.0.0a7)

--- mapping/dwellingFabricMapper.test.ts
+++ mapping/dwellingFabricMapper.test.ts
@@ -1160,11 +1160,11 @@ describe("dwelling fabric mapper", () => {
 					distance: 9,
 					height: 10,
 				},
-				{
-					type: "reveal",
-					depth: 200,
-					distance: 100,
-				},
+				// {
+				// 	type: "reveal",
+				// 	depth: 200,
+				// 	distance: 100,
+				// },
 			],
 			security_risk: false,
 			u_value: externalGlazedDoor.uValue,
@@ -1352,11 +1352,11 @@ describe("dwelling fabric mapper", () => {
 					distance: 9,
 					height: 10,
 				},
-				{
-					type: "reveal",
-					depth: 200,
-					distance: 100,
-				},
+				// {
+				// 	type: "reveal",
+				// 	depth: 200,
+				// 	distance: 100,
+				// },
 			],
 		};
 		expect(windowElement).toEqual(expectedWindow);
